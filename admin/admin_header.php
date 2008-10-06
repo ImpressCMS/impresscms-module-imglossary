54cc69fa558a9f8de82195bafe10c20cb336f69d
Ronald <pietjebell31@hotmail.com>
2008-10-06 22:58:00 +0000

    Adding imGlossary to SVN
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imglossary/trunk/modules/imglossary@5429 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ admin/admin_header.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * $Id: admin_header.php v 1.0 8 May 2004 hsalazar Exp $
+ * Module: Wordbook - a multicategory glossary
+ * Version: v 1.00
+ * Release Date: 8 May 2004
+ * Author: hsalazar
+ * Licence: GNU
+ */
+
+include '../../../mainfile.php';
+include '../../../include/cp_header.php';
+
+$glossdirname = basename( dirname( dirname( __FILE__ ) ) );
+
+// Commented this part out because I think it's not needed here (McDonald)
+//if ( file_exists("../language/".$xoopsConfig['language']."/main.php") ) 
+//	{
+//	include "../language/".$xoopsConfig['language']."/main.php";
+//	}
+//else 
+//	{
+//	include "../language/english/main.php";
+//	}
+
+include_once XOOPS_ROOT_PATH . '/modules/' . $glossdirname . '/include/functions.php';
+
+include_once XOOPS_ROOT_PATH . '/class/xoopstree.php';
+include_once XOOPS_ROOT_PATH . '/class/xoopslists.php';
+include_once XOOPS_ROOT_PATH . '/class/xoopsformloader.php';
+include_once XOOPS_ROOT_PATH . '/kernel/module.php';
+$myts =& MyTextSanitizer::getInstance();
+
+if ( is_object( $xoopsUser) ) {
+	$xoopsModule = XoopsModule::getByDirname( $glossdirname );
+	if ( !$xoopsUser -> isAdmin( $xoopsModule -> mid() ) ) {
+		redirect_header( XOOPS_URL . "/", 1, _NOPERM );
+		exit();
+	}
+} else {
+	redirect_header ( XOOPS_URL . "/", 1, _NOPERM );
+	exit();
+}
+
+?>
---
+++ admin/menu.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * $Id: menu.php v 1.0 8 May 2004 hsalazar Exp $
+ * Module: Soapbox
+ * Version: v 1.00
+ * Release Date: 8 May 2004
+ * Author: hsalazar
+ * Licence: GNU
+ */
+
+$adminmenu[0]['title'] = _MI_WB_ADMENU1;
+$adminmenu[0]['link'] = "admin/index.php";
+$adminmenu[1]['title'] = _MI_WB_ADMENU2;
+$adminmenu[1]['link'] = "admin/category.php";
+$adminmenu[2]['title'] = _MI_WB_ADMENU3;
+$adminmenu[2]['link'] = "admin/entry.php";
+$adminmenu[4]['title'] = _MI_WB_ADMENU4;
+$adminmenu[4]['link'] = "admin/myblocksadmin.php";
+$adminmenu[5]['title'] = _MI_WB_ADMENU5;
+$adminmenu[5]['link'] = "index.php";
+$adminmenu[6]['title'] = _MI_WB_ADMENU6;
+$adminmenu[6]['link'] = "admin/importdictionary091.php";
+?>
