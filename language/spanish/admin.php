2d269dd45d321a5bc20d36c0e33f50740f2beb82
Juan Calzado <debianus@impresscms.org>
2008-10-10 21:24:37 +0000

    Updating the Spanish translation for imglossary
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imglossary/trunk/modules/imglossary@5555 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- language/spanish/admin.php
+++ language/spanish/admin.php
@@ -128,6 +128,6 @@ define("_AM_WB_IMPDICT_11","Volver a la administración");
 define("_AM_WB_NOCOLEXISTS", "Lo sentimos: no hay aún categorías definidas.<br />Por favor, contacte con el administrador del sitio para comentarle esta circunstancia."); // This was originally in the main.php file
 
 // imGlossary v1.00
-define("_AM_WB_SHOWOFFLINE", "Offline");
+define("_AM_WB_SHOWOFFLINE", "Mostrar sin conexión");
 
 ?>
