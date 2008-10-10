878f7813de84fc2967175b1c87949ecc23442ca7
Ronald <pietjebell31@hotmail.com>
2008-10-10 08:49:06 +0000

    Swapped Add Category and Add Entry in menu
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imglossary/trunk@5537 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- modules/imglossary/admin/menu.php
+++ modules/imglossary/admin/menu.php
@@ -11,9 +11,9 @@
 $adminmenu[0]['title'] = _MI_WB_ADMENU1;
 $adminmenu[0]['link'] = "admin/index.php";
 $adminmenu[1]['title'] = _MI_WB_ADMENU2;
-$adminmenu[1]['link'] = "admin/category.php";
+$adminmenu[1]['link'] = "admin/entry.php";
 $adminmenu[2]['title'] = _MI_WB_ADMENU3;
-$adminmenu[2]['link'] = "admin/entry.php";
+$adminmenu[2]['link'] = "admin/category.php";
 $adminmenu[4]['title'] = _MI_WB_ADMENU4;
 $adminmenu[4]['link'] = "admin/myblocksadmin.php";
 $adminmenu[5]['title'] = _MI_WB_ADMENU5;
