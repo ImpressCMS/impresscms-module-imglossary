3222ec26538219371556cf34751fc7b0db7c5f7e
GibaPhp <gibaphp.xoopstotal@gmail.com>
2008-10-13 22:22:35 +0000

    portuguesebr - imGlossary - Step 1 - all files with base in english. Started translation... format utf-8...
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imglossary/trunk/modules/imglossary@5648 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ language/portuguesebr/blocks.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * $Id: blocks.php v 1.0 8 May 2004 hsalazar Exp $
+ * Module: Wordbook - a multicategory glossary
+ * Version: v 1.00
+ * Release Date: 8 May 2004
+ * Author: hsalazar
+ * Licence: GNU
+ */
+// Blocks
+define("_MB_IMGLOSSARY_DATE", "Published date");
+define("_MB_IMGLOSSARY_DELTERM", "Delete this term");
+define("_MB_IMGLOSSARY_DISP", "Display");
+define("_MB_IMGLOSSARY_EDITTERM", "Edit this term");
+define("_MB_IMGLOSSARY_HITS", "Number of hits");
+define("_MB_IMGLOSSARY_INTENTRY", "Have a look at this entry at %s");
+define("_MB_IMGLOSSARY_INTENTRYFOUND", "Here is an interesting entry I have found at %s");
+define("_MB_IMGLOSSARY_ORDER", "Display order");
+define("_MB_IMGLOSSARY_PRINTTERM", "Print in friendly format");
+define("_MB_IMGLOSSARY_RANDOMTITLE", "Random term");
+define("_MB_IMGLOSSARY_SEEMORE", "More definitions...");
+define("_MB_IMGLOSSARY_SENDTOFRIEND", "Send this term to a friend");
+define("_MB_IMGLOSSARY_TERMS", "terms");
+define("_MB_IMGLOSSARY_TITLE", "Title");
+define("_MB_IMGLOSSARY_WEIGHT", "List by weight");
+
+// imGlossary v1.00
+define( '_MB_IMGLOSSARY_DATEFORMAT', 'Date format: ' );
+define( '_MB_IMGLOSSARY_DATEFORMATMANUAL', 'See <a href="http://jp.php.net/manual/en/function.date.php" target="_blank">PHP manual</a>' );
+define( '_MB_IMGLOSSARY_CATEGORY', 'Category: ' );
+define( '_MB_IMGLOSSARY_ADMININDEX', 'Administration' );
+?>
