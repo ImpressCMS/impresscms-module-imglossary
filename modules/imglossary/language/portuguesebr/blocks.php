eb8c78b53f3a1e8ae84c6e64da6bbedd6b505ea9
GibaPhp <gibaphp.xoopstotal@gmail.com>
2008-10-13 23:09:20 +0000

    portuguesebr - imGlossary - Step 3 - blocks.php finished this file translation... with format utf-8...
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imglossary/trunk@5652 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- modules/imglossary/language/portuguesebr/blocks.php
+++ modules/imglossary/language/portuguesebr/blocks.php
@@ -8,25 +8,25 @@
  * Licence: GNU
  */
 // Blocks
-define("_MB_IMGLOSSARY_DATE", "Published date");
-define("_MB_IMGLOSSARY_DELTERM", "Delete this term");
-define("_MB_IMGLOSSARY_DISP", "Display");
-define("_MB_IMGLOSSARY_EDITTERM", "Edit this term");
-define("_MB_IMGLOSSARY_HITS", "Number of hits");
-define("_MB_IMGLOSSARY_INTENTRY", "Have a look at this entry at %s");
-define("_MB_IMGLOSSARY_INTENTRYFOUND", "Here is an interesting entry I have found at %s");
-define("_MB_IMGLOSSARY_ORDER", "Display order");
-define("_MB_IMGLOSSARY_PRINTTERM", "Print in friendly format");
-define("_MB_IMGLOSSARY_RANDOMTITLE", "Random term");
-define("_MB_IMGLOSSARY_SEEMORE", "More definitions...");
-define("_MB_IMGLOSSARY_SENDTOFRIEND", "Send this term to a friend");
-define("_MB_IMGLOSSARY_TERMS", "terms");
-define("_MB_IMGLOSSARY_TITLE", "Title");
-define("_MB_IMGLOSSARY_WEIGHT", "List by weight");
+define("_MB_IMGLOSSARY_DATE", "Data de Publicação");
+define("_MB_IMGLOSSARY_DELTERM", "Apagar este Termo");
+define("_MB_IMGLOSSARY_DISP", "Mostrar");
+define("_MB_IMGLOSSARY_EDITTERM", "Editar este Termo");
+define("_MB_IMGLOSSARY_HITS", "Número de Acessos");
+define("_MB_IMGLOSSARY_INTENTRY", "Dê uma olhada nesta entrada na %s");
+define("_MB_IMGLOSSARY_INTENTRYFOUND", "Aqui está um interessante termo que encontrei no %s");
+define("_MB_IMGLOSSARY_ORDER", "Mostrar ordem");
+define("_MB_IMGLOSSARY_PRINTTERM", "Imprimir em formato amigável");
+define("_MB_IMGLOSSARY_RANDOMTITLE", "Termo Aleatório");
+define("_MB_IMGLOSSARY_SEEMORE", "Mais definições...");
+define("_MB_IMGLOSSARY_SENDTOFRIEND", "Envie este termo para um amigo");
+define("_MB_IMGLOSSARY_TERMS", "termos");
+define("_MB_IMGLOSSARY_TITLE", "Título");
+define("_MB_IMGLOSSARY_WEIGHT", "Lista por peso");
 
 // imGlossary v1.00
-define( '_MB_IMGLOSSARY_DATEFORMAT', 'Date format: ' );
-define( '_MB_IMGLOSSARY_DATEFORMATMANUAL', 'See <a href="http://jp.php.net/manual/en/function.date.php" target="_blank">PHP manual</a>' );
-define( '_MB_IMGLOSSARY_CATEGORY', 'Category: ' );
-define( '_MB_IMGLOSSARY_ADMININDEX', 'Administration' );
+define( '_MB_IMGLOSSARY_DATEFORMAT', 'Formato de Data: ' );
+define( '_MB_IMGLOSSARY_DATEFORMATMANUAL', 'Veja no <a href="http://jp.php.net/manual/pt_BR/function.date.php" target="_blank">manual do PHP</a>' );
+define( '_MB_IMGLOSSARY_CATEGORY', 'Categoria: ' );
+define( '_MB_IMGLOSSARY_ADMININDEX', 'Administração' );
 ?>
