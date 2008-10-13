10fcc438d3e6133399b77e5b5b451eeeb3bff5df
Ronald <pietjebell31@hotmail.com>
2008-10-13 23:56:07 +0000

    Removed searchform code from index.php and use imglossary_showSearchForm() instead
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imglossary/trunk@5655 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- modules/imglossary/index.php
+++ modules/imglossary/index.php
@@ -53,29 +53,7 @@ switch ( $op ) {
 		}
 
 		// To display the search form
-		$searchform = '<table width="100%">';
-		$searchform .= '<form name="op" id="op" action="search.php" method="post">';
-		$searchform .= '<tr><td style="text-align: right; line-height: 200%;">';
-		$searchform .= _MD_IMGLOSSARY_LOOKON . '</td><td width="10">&nbsp;</td><td style="text-align: left;">';
-		$searchform .= '<select name="type"><option value="1">' . _MD_IMGLOSSARY_TERMS . '</option>';
-		$searchform .= '<option value="2">' . _MD_IMGLOSSARY_DEFINS . '</option>';
-		$searchform .= '<option value="3">' . _MD_IMGLOSSARY_TERMSDEFS . '</option></select></td></tr>';
-		if ( $xoopsModuleConfig['multicats'] == 1 ) {
-			$searchform .= '<tr><td style="text-align: right; line-height: 200%">' . _MD_IMGLOSSARY_CATEGORY . '</td>';
-			$searchform .= '<td>&nbsp;</td><td style="text-align: left;">';
-			$resultcat = $xoopsDB -> query( "SELECT categoryID, name FROM " . $xoopsDB -> prefix( 'imglossary_cats' ) . " ORDER BY categoryID" );
-			$searchform .= '<select name="categoryID">';
-			$searchform .= '<option value="0">' . _MD_IMGLOSSARY_ALLOFTHEM . '</option>';
-			while ( list( $categoryID, $name ) = $xoopsDB -> fetchRow( $resultcat ) ) {
-				$searchform .= '<option value="' . $categoryID . '">' . $categoryID . ' : ' . $name . '</option>';
-			}
-			$searchform .= '</select></td></tr>';
-		}
-		$searchform .= '<tr><td style="text-align: right; line-height: 200%;">';
-		$searchform .= _MD_IMGLOSSARY_TERM . '</td><td>&nbsp;</td><td style="text-align: left;">';
-		$searchform .= '<input type="text" name="term" /></td></tr><tr>';
-		$searchform .= '<td>&nbsp;</td><td>&nbsp;</td><td><input type="submit" value="' . _MD_IMGLOSSARY_SEARCH . '" />';
-		$searchform .= '</td></tr></form></table>';
+		$searchform = imglossary_showSearchForm( '' );
 		$xoopsTpl -> assign( 'searchform', $searchform );
 
 		// To display the linked letter list
