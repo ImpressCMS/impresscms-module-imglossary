62630adba1eeaf50e89a1f0c91adabf80b2bb316
GibaPhp <gibaphp.xoopstotal@gmail.com>
2008-10-14 11:04:28 +0000

    portuguesebr - imGlossary - Step 6 - revision general  translation... with format utf-8...Finished...
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imglossary/trunk/modules/imglossary@5664 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- language/portuguesebr/modinfo.php
+++ language/portuguesebr/modinfo.php
@@ -26,43 +26,43 @@ define("_MI_IMGLOSSARY_RANDOMTERM", "Termo Randômico");
 
 // A brief description of this module
 define("_MI_IMGLOSSARY_ALLOWSUBMIT", "Usuários podem enviar termos?");
-define("_MI_IMGLOSSARY_ALLOWSUBMITDSC", "Se escolher <em>Sim</em>, os usuários terão acesso a um formulário para apresentar novos termos no glossário");
+define("_MI_IMGLOSSARY_ALLOWSUBMITDSC", "Se escolher <em><b>Sim</b></em>, os usuários terão acesso a um formulário para apresentar novos termos no glossário");
 
 define("_MI_IMGLOSSARY_ANONSUBMIT", "Visitantes/Anônimos podem apresentar novos termos?");
-define("_MI_IMGLOSSARY_ANONSUBMITDSC", "Se escolher <em>Sim</em>, visitantes/anônimos irão ter acesso a um formulário para o envio de novos termos no glossário");
+define("_MI_IMGLOSSARY_ANONSUBMITDSC", "Se escolher <em><b>Sim</b></em>, visitantes/anônimos irão ter acesso a um formulário para o envio de novos termos no glossário");
 
 define("_MI_IMGLOSSARY_DATEFORMAT", "Em que formato deverá mostrar a data?");
 define("_MI_IMGLOSSARY_DATEFORMATDSC", "Use a parte final da language/english/global.php para selecionar um tipo de estilo de data.<br />Veja no <a href='http://jp.php.net/manual/pt_BR/function.date.php' target='_blank'>Manual do PHP</a>");
 
 define("_MI_IMGLOSSARY_PERPAGE", "Qual o Número de termos por página (no lado do Admin)?");
-define("_MI_IMGLOSSARY_PERPAGEDSC", "Número de entradas que será mostrado de uma vez na tabela que exibe as entradas ativas no lado do admin.");
+define("_MI_IMGLOSSARY_PERPAGEDSC", "Número de <b>termos</b> que será mostrado de uma vez na tabela que exibe as entradas ativas no lado do admin.");
 
-define("_MI_IMGLOSSARY_PERPAGEINDEX", "Qual o Número de termos por página (No lado do Usuário)?");
+define("_MI_IMGLOSSARY_PERPAGEINDEX", "Qual o Número de <b>termos</b> por página (No lado do Usuário)?");
 define("_MI_IMGLOSSARY_PERPAGEINDEXDSC", "Informe o Número de termos que será mostrado em cada página no lado do usuário na parte lateral do módulo.");
 
 define("_MI_IMGLOSSARY_AUTOAPPROVE", "Aprovar automaticamente termos?");
-define("_MI_IMGLOSSARY_AUTOAPPROVEDSC", "Se responder com <em>Sim</em>, Será atualizada e publicada a nova informação dentro do ImpressCMS sem a intervenção do admin deste módulo.");
+define("_MI_IMGLOSSARY_AUTOAPPROVEDSC", "Se responder com <em><b>Sim</b></em>, Será atualizada e publicada a nova informação dentro do ImpressCMS sem a intervenção do admin deste módulo.");
 
 define("_MI_IMGLOSSARY_MULTICATS", "Você quer ter categorias neste glossário?");
-define("_MI_IMGLOSSARY_MULTICATSDSC", "Se escolheu a opção <em>Sim</em>, permitirá que seja montado categorias no seu glossário.  Se ficar definido como não, vai ter uma única categoria automáticamente para todos os seus termos.");
+define("_MI_IMGLOSSARY_MULTICATSDSC", "Se escolheu a opção <em><b>Sim</b></em>, permitirá que seja montado categorias no seu glossário.  Se ficar definido como não, vai ter uma única categoria automáticamente para todos os seus termos.");
 
 define("_MI_IMGLOSSARY_CATSINMENU","As categorias devem ser mostradas no menu?"); 
-define("_MI_IMGLOSSARY_CATSINMENUDSC","Se informar <em>Sim</em> irá receber em seu menu principal todas as categorias que existirem dentro de seu módulo de glossário."); 
+define("_MI_IMGLOSSARY_CATSINMENUDSC","Se informar <em><b>Sim</b></em> irá receber em seu menu principal todas as categorias que existirem dentro de seu módulo de glossário."); 
 
 define("_MI_IMGLOSSARY_CATSPERINDEX","Número de categorias por página (Lado do Usuário)?"); 
 define("_MI_IMGLOSSARY_CATSPERINDEXDSC","Isto irá definir quantas categorias vão ser mostradas na página principal."); 
 
 define("_MI_IMGLOSSARY_ALLOWADMINHITS", "Irá incluir o admin na contagem de acessos?");
-define("_MI_IMGLOSSARY_ALLOWADMINHITSDSC", "Se escolher esta opção com <em>Sim</em>, aumentará o contador para cada novo acesso do admin ao fazer visitas. Normalmente o Admin deve ser deixado como não.");
+define("_MI_IMGLOSSARY_ALLOWADMINHITSDSC", "Se escolher esta opção com <em><b>Sim</b></em>, aumentará o contador para cada novo acesso do admin ao fazer visitas. Normalmente o Admin deve ser deixado como não.");
 
 define("_MI_IMGLOSSARY_MAILTOADMIN", "Enviar e-mail para o admin a cada novo termo enviado?");  
-define("_MI_IMGLOSSARY_MAILTOADMINDSC", "Se marcar esta opção com <em>Sim</em>, o gestor deste módulo irá receber um e-mail a cada novo termo que for apresentado."); 
+define("_MI_IMGLOSSARY_MAILTOADMINDSC", "Se marcar esta opção com <em><b>Sim</b></em>, o gestor deste módulo irá receber um e-mail a cada novo termo que for apresentado."); 
  
 define("_MI_IMGLOSSARY_RANDOMLENGTH", "Qual o Comprimento de caracteres mostrar no bloco de definições aleatórias?");  
 define("_MI_IMGLOSSARY_RANDOMLENGTHDSC", "Qual a quantidade de caracteres que você deseja mostrar no bloco de termos aleatórios, tanto na página principal e no bloco? (O Padrão é: 500)");
 
 define("_MI_IMGLOSSARY_LINKTERMS", "Mostrar link de outros termos nas definições do glossário?");  
-define("_MI_IMGLOSSARY_LINKTERMSDSC", "Se responder <em>Sim</em>, imGlossary irá vincular automaticamente estas definições em seus termos caso estes já existam em seu glossário.");
+define("_MI_IMGLOSSARY_LINKTERMSDSC", "Se responder <em><b>Sim</b></em>, imGlossary irá vincular automaticamente estas definições em seus termos caso estes já existam em seu glossário.");
 
 // Names of admin menu items
 define("_MI_IMGLOSSARY_ADMENU1", "Principal");
@@ -84,9 +84,9 @@ define("_MI_IMGLOSSARY_TITLE", "Título");
 define("_MI_IMGLOSSARY_WEIGHT", "Peso");
 define("_MI_IMGLOSSARY_ADMENU7", "Participações");
 define("_MI_IMGLOSSARY_SHOWSUBMITTER", "Mostrar quem enviou o Termo?");
-define("_MI_IMGLOSSARY_SHOWSUBMITTERDSC", "Escolha <em>Sim</em> para mostrar quem foi o responsável por cada novo termo enviado.");
+define("_MI_IMGLOSSARY_SHOWSUBMITTERDSC", "Escolha <em><b>Sim</b></em> para mostrar quem foi o responsável por cada novo termo enviado.");
 define("_MI_IMGLOSSARY_SHOWSBOOKMARKS","Mostrar um BookMark Social em cada termo?");
-define("_MI_IMGLOSSARY_SHOWSBOOKMARKSDSC", "Escolha <em>Sim</em> para ter um ícone de BookMark Social visível em cada termo.");
+define("_MI_IMGLOSSARY_SHOWSBOOKMARKSDSC", "Escolha <em><b>Sim</b></em> para ter um ícone de BookMark Social visível em cada termo.");
 define("_MI_IMGLOSSARY_SEARCHCOLOR", "Cor de fundo para os termos da pesquisa:");
 define("_MI_IMGLOSSARY_SEARCHCOLORDSC", "Introduza a cor de fundo para usar como termos de pesquisa. Padrão: <span style='background-color: #FFFF00;'>&nbsp;#FFFF00&nbsp;</span>");
 ?>
