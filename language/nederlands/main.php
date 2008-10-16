10b3da9bfda52a2662a63eb5a35eca5aaaffb438
Ronald <pietjebell31@hotmail.com>
2008-10-16 00:23:54 +0000

    Finshed translation to dutch (utf8)
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imglossary/trunk/modules/imglossary@5713 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- language/nederlands/main.php
+++ language/nederlands/main.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * $Id: main.php v 1.0 8 May 2004 hsalazar Exp $
- * Module: Wordbook - a multicategory glossary
- * Version: v 1.00
- * Release Date: 8 May 2004
- * Author: hsalazar
+ * $Id: main.php
+ * Module: imGlossary - a multicategory glossary
+ * Author: McDonald
+ * Language: Dutch (Nederlands)
  * Licence: GNU
  */
+ 
 define("_MD_IMGLOSSARY_NOCOLEXISTS", "Sorry, er zijn nog geen categorieën gedefinieërd.<br />Neemt u alstublieft kontakt op met de administrator om hem/haar hierover te informeren.");
 define("_MD_IMGLOSSARY_ALL", "Alles");
 define("_MD_IMGLOSSARY_ALLCATS", "Alle categorieën");
@@ -21,7 +21,7 @@ define("_MD_IMGLOSSARY_ASKFORDEF", "Vraag een definitie aan");
 define("_MD_IMGLOSSARY_BEGINWITHLETTER", "termen die beginnen met deze letter.");
 define("_MD_IMGLOSSARY_BROWSECAT", "Zoek per categorie");
 define("_MD_IMGLOSSARY_BROWSELETTER", "Zoek per letter");
-define("_MD_IMGLOSSARY_CATEGORY", "Zoek in");
+define("_MD_IMGLOSSARY_CATEGORY", "Categorie");
 define("_MD_IMGLOSSARY_CATS", "Categorieën: ");
 define("_MD_IMGLOSSARY_COUNT", "Deze term is %s keer bekeken.");
 define("_MD_IMGLOSSARY_CREATE", "Zend de term in");
@@ -51,88 +51,88 @@ define("_MD_IMGLOSSARY_INTENTRYFOUND", "Hier is een interessante term die ik heb
 define("_MD_IMGLOSSARY_INTROREQUEST", "U hebt de door u gezochte definitie niet gevonden? Zend de term in door dit formulier in te vullen. We zullen de bijbehorende definitie opzoeken en aan onze database toevoegen.");
 define("_MD_IMGLOSSARY_LOOKON", "Zoek");
 define("_MD_IMGLOSSARY_MAINHEAD", "Welkom bij %s");
-define("_MD_IMGLOSSARY_MAININTRO", "In this area of the site you will find our catalogue of glossary entries, as well as the latest entry from each of our categories. Click on a category's name to see all the entries associated with that category, or on an entry's name to read directly the entry. Depending on privileges, you can rate each entry, select notification options or leave your comments.");
-define("_MD_IMGLOSSARY_MAINNOSELECTCAT", "You did not select a valid category");
-define("_MD_IMGLOSSARY_MAINNOTOPICS", "There are no entries in this category");
-define("_MD_IMGLOSSARY_MESSAGESENT", "Message to %s sent.");
+define("_MD_IMGLOSSARY_MAININTRO", "Hier vindt u de termen in onze woordenlijst, en tevens de laatste term van onze categorieën. Klik op de naam van een categorie om alle termen te zien die tot deze categorie behoren, of klik op een term om direkt de betekenis ervan te lezen. Afhankelijk van de ingestelde privileges, kunt u een reaktie achterlaten.");
+define("_MD_IMGLOSSARY_MAINNOSELECTCAT", "U heeft geen geldige categorie gekozen.");
+define("_MD_IMGLOSSARY_MAINNOTOPICS", "Er zijn geen termen in deze categorie.");
+define("_MD_IMGLOSSARY_MESSAGESENT", "Bericht naar %s verzonden.");
 define("_MD_IMGLOSSARY_NO", "Nee");
-define("_MD_IMGLOSSARY_NOENTRIESINCAT", "There are no entries yet in this glossary.");
-define("_MD_IMGLOSSARY_NORESULTS", "There were no matches for the search term given. Please use another word and try again.");
-define("_MD_IMGLOSSARY_NOSEARCHTERM", "You have not declared a search term. Please do it now in the search form included in this page.");
-define("_MD_IMGLOSSARY_NOTERMSINLETTER", "There are no entries that begin with this letter.");
-define("_MD_IMGLOSSARY_NOTIFY", "Notify on publish?");
-define("_MD_IMGLOSSARY_NOTIFYONPUB", "The user has asked to be notified when we do something about the definition.<br />");
+define("_MD_IMGLOSSARY_NOENTRIESINCAT", "Er zijn geen termen in deze woordenlijst.");
+define("_MD_IMGLOSSARY_NORESULTS", "Er zijn geen resultaten voor de opgegeven term gevonden. Gebruik een ander woord en probeer het opnieuw.");
+define("_MD_IMGLOSSARY_NOSEARCHTERM", "U heeft geen zoekterm opgegeven. U kunt dit nu doen in het formulier op deze pagina.");
+define("_MD_IMGLOSSARY_NOTERMSINLETTER", "Er zijn geen termen die met deze letter beginnen.");
+define("_MD_IMGLOSSARY_NOTIFY", "Bericht bij publikatie?");
+define("_MD_IMGLOSSARY_NOTIFYONPUB", "De bezoeker heeft om bericht gevraagd, indien de definitie behandelt wordt.<br />");
 define("_MD_IMGLOSSARY_OTHER", "Overige");
-define("_MD_IMGLOSSARY_POPULARENT", "Popular entries");
-define("_MD_IMGLOSSARY_POSTED", "Published");
-define("_MD_IMGLOSSARY_PRINTERFRIENDLY", "Print this entry in a printer friendly format");
-define("_MD_IMGLOSSARY_PRINTTERM", "Print in friendly format");
-define("_MD_IMGLOSSARY_RANDOMTERM", "Random term");
-define("_MD_IMGLOSSARY_RECEIVED", "The entry you sent has been received. We'll look at it as soon as possible.");
-define("_MD_IMGLOSSARY_RECEIVEDANDAPPROVED", "The entry you sent has been received. Since the auto-approve option is on, we'll publish it at once.");
-define("_MD_IMGLOSSARY_RECENTENT", "Recent entries");
-define("_MD_IMGLOSSARY_REFERENCE", "Reference<span style='font-size: xx-small; font-weight: normal; display: block;'>(Optionally, you may give the source of your definition here.)</span>");
-define("_MD_IMGLOSSARY_REQRCVD", "Your request has been received. We'll look at it as soon as possible.");
-define("_MD_IMGLOSSARY_REQSEND", "Request sent");
-define("_MD_IMGLOSSARY_REQTERM", "[*] Term to define");
-define("_MD_IMGLOSSARY_REQUESTDEF", "Request a definition");
-define("_MD_IMGLOSSARY_REQUESTFORM", "Request a definition");
-define("_MD_IMGLOSSARY_REQUESTSENT", "Your request has been forwarded to the webmaster of %s.");
-define("_MD_IMGLOSSARY_RETURN", "Return");
-define("_MD_IMGLOSSARY_RETURN2INDEX", "Return to module index");
-define("_MD_IMGLOSSARY_SEARCH", "Search!");
-define("_MD_IMGLOSSARY_SEARCHENTRY", "Search");
-define("_MD_IMGLOSSARY_SEARCHHEAD", "Search results");
-define("_MD_IMGLOSSARY_SECTIONNAME", "Glossary");
-define("_MD_IMGLOSSARY_SEEMORE", "More definitions...");
-define("_MD_IMGLOSSARY_SENDSTORY", "Send this entry to a friend");
-define("_MD_IMGLOSSARY_SENDTOFRIEND", "Send this term to a friend");
-define("_MD_IMGLOSSARY_SENT", "Submitted on: ");
-define("_MD_IMGLOSSARY_SENTCONFIRMMAIL", "Your comments have been sent to <b>%s</b> as a confirmation email.");
-define("_MD_IMGLOSSARY_STILLNOTHINGHERE", "<h4 style='margin: 0;'>Glossaries empty</h4>Sorry: We don't have any entries in our glossaries yet. You can help us by requesting some entries whose definition you'd like to know. If you're a registered user, you can also help by submitting some terms whose definition you know.");
-define("_MD_IMGLOSSARY_SUB_SMNAME", "Fill this form to send your entry");
-define("_MD_IMGLOSSARY_SUB_SNEWNAME", "Submit an entry to %s");
-define("_MD_IMGLOSSARY_SUB_SNEWNAMEDESC", "please fill the following form to submit your new entry to our site. As you're well aware, it'll have to be validated by an administrator before it gets published. However, if the administrator has activated the auto-approve setting, your entry will be published instantly.");
-define("_MD_IMGLOSSARY_SUBMIT", "Submit request");
-define("_MD_IMGLOSSARY_SUBMITART", "Submit an entry");
-define("_MD_IMGLOSSARY_SUBMITENTRY", "Submit an entry");
-define("_MD_IMGLOSSARY_SUBMITTED", "Submitted by %s. | ");
-define("_MD_IMGLOSSARY_SUBMITTER", "Submitter: ");
-define("_MD_IMGLOSSARY_TERM", "Search term");
+define("_MD_IMGLOSSARY_POPULARENT", "Populaire termen");
+define("_MD_IMGLOSSARY_POSTED", "Gepubliceerd");
+define("_MD_IMGLOSSARY_PRINTERFRIENDLY", "Print deze term");
+define("_MD_IMGLOSSARY_PRINTTERM", "Print");
+define("_MD_IMGLOSSARY_RANDOMTERM", "Willekeurige term");
+define("_MD_IMGLOSSARY_RECEIVED", "De door u ingezonden term is ontvangen. We zullen het zo snel mogelijk in behandeling nemen.");
+define("_MD_IMGLOSSARY_RECEIVEDANDAPPROVED", "De door u ingezonden term is ontvangen. Indien de instelling <em>automatische goedkeuring</em> aanstaat, wordt de term meteen gepubliceerd.");
+define("_MD_IMGLOSSARY_RECENTENT", "Recente termen");
+define("_MD_IMGLOSSARY_REFERENCE", "Referentie<span style='font-size: xx-small; font-weight: normal; display: block;'>(Optioneel, u kunt hier de bron van uw definitie opgeven.)</span>");
+define("_MD_IMGLOSSARY_REQRCVD", "Uw aanvraag is ontvangen. We zullen het zo snel mogelijk in behandeling nemen.");
+define("_MD_IMGLOSSARY_REQSEND", "Aanvraag verzonden");
+define("_MD_IMGLOSSARY_REQTERM", "[*] Te definieëren term");
+define("_MD_IMGLOSSARY_REQUESTDEF", "Aanvragen definitie");
+define("_MD_IMGLOSSARY_REQUESTFORM", "Vraag een definitie aan");
+define("_MD_IMGLOSSARY_REQUESTSENT", "Uw aanvraag is doorgestuurd naar de webmaster van %s.");
+define("_MD_IMGLOSSARY_RETURN", "Terug");
+define("_MD_IMGLOSSARY_RETURN2INDEX", "Terug naar module index");
+define("_MD_IMGLOSSARY_SEARCH", "Zoek");
+define("_MD_IMGLOSSARY_SEARCHENTRY", "Zoek");
+define("_MD_IMGLOSSARY_SEARCHHEAD", "Zoek resultaten");
+define("_MD_IMGLOSSARY_SECTIONNAME", "Woordenlijst");
+define("_MD_IMGLOSSARY_SEEMORE", "Meer definities...");
+define("_MD_IMGLOSSARY_SENDSTORY", "Stuur deze definitie naar een vriend");
+define("_MD_IMGLOSSARY_SENDTOFRIEND", "Stuur deze term naar een vriend");
+define("_MD_IMGLOSSARY_SENT", "Ingezonden op: ");
+define("_MD_IMGLOSSARY_SENTCONFIRMMAIL", "Uw reakties zijn verstuurd naar <b>%s</b> als een confirmatie email.");
+define("_MD_IMGLOSSARY_STILLNOTHINGHERE", "<h4 style='margin: 0;'>Woordenlijsten leeg</h4>Sorry: We hebben nog geen termen in onze woordenlijst. U kunt ons helpen door een term waarvan u de definitie wilt weten aan te vragen. Als u een geregistreerde bezoeker bent, kunt u ons helpen door enkele termen waarvan u de definitie weet in te sturen.");
+define("_MD_IMGLOSSARY_SUB_SMNAME", "Vul dit formulier in om uw term in te sturen");
+define("_MD_IMGLOSSARY_SUB_SNEWNAME", "Stuur een term naar %s");
+define("_MD_IMGLOSSARY_SUB_SNEWNAMEDESC", "vul het volgende formulier in om een nieuwe term voor onze website in te zenden. De inzending dient eerst te worden goedgekeurd door een websitebeheerder, voordat het gepubliceerd wordt. Mocht de websitebeheerder de optie voor automatische goedkeuring hebben ingeschakelt, dan wordt uw inzending meteen gepubliceerd.");
+define("_MD_IMGLOSSARY_SUBMIT", "Inzenden aanvraag");
+define("_MD_IMGLOSSARY_SUBMITART", "Inzenden term");
+define("_MD_IMGLOSSARY_SUBMITENTRY", "Inzenden term");
+define("_MD_IMGLOSSARY_SUBMITTED", "Ingezonden door %s. | ");
+define("_MD_IMGLOSSARY_SUBMITTER", "Inzender: ");
+define("_MD_IMGLOSSARY_TERM", "Zoek term");
 define("_MD_IMGLOSSARY_TERMS", "Termen");
-define("_MD_IMGLOSSARY_TERMSDEFS", "Terms and definitions");
-define("_MD_IMGLOSSARY_THANKS1", "Thank you for your participation!<br />");
-define("_MD_IMGLOSSARY_THANKS2", "Thank you for requesting a definition!");
-define("_MD_IMGLOSSARY_THANKYOU", "Thank you for taking the time to request a definition that will enrich the glossaries of %s.");
-define("_MD_IMGLOSSARY_THEREWERE", "There were %s entries matching the search term: <b>%s</b>.");
-define("_MD_IMGLOSSARY_TIMESREAD", "Times read");
-define("_MD_IMGLOSSARY_UNKNOWNERROR", "ERROR.  Returning you to where you were!");
-define("_MD_IMGLOSSARY_URL", "URL<span style='font-size: xx-small; font-weight: normal; display: block;'>(Please enter a valid URL with or without the HTTP prefix.)</span>");
+define("_MD_IMGLOSSARY_TERMSDEFS", "Termen en definities");
+define("_MD_IMGLOSSARY_THANKS1", "Bedankt voor uw medewerking!<br />");
+define("_MD_IMGLOSSARY_THANKS2", "Bedankt voor het aanvragen van een definitie!");
+define("_MD_IMGLOSSARY_THANKYOU", "Bedankt voor het nemen van de moeite om een definitie aan te vragen, dat de woordenlijsten van %s zal verrijken.");
+define("_MD_IMGLOSSARY_THEREWERE", "Er waren %s termen die overeenkwamen met de zoekterm: <b>%s</b>.");
+define("_MD_IMGLOSSARY_TIMESREAD", "Aantal keer gelezen");
+define("_MD_IMGLOSSARY_UNKNOWNERROR", "FOUT.  U wordt teruggebracht naar waar u vandaan kwam!");
+define("_MD_IMGLOSSARY_URL", "URL<span style='font-size: xx-small; font-weight: normal; display: block;'>(Vul een geldige URL in met of zonder het HTTP voorvoegsel.)</span>");
 define("_MD_IMGLOSSARY_USERMAIL", "E-mail");
 define("_MD_IMGLOSSARY_USERNAME", "Naam");
 define("_MD_IMGLOSSARY_WEBMASTER", "Webmaster");
 define("_MD_IMGLOSSARY_WEHAVE", "We hebben momenteel");
-define("_MD_IMGLOSSARY_WHOASKED", "%s asked us to find the definition for: ");
-define("_MD_IMGLOSSARY_WHOSUBMITTED", "%s has submitted the definition for this term: ");
-define("_MD_IMGLOSSARY_YES", "Yes");
+define("_MD_IMGLOSSARY_WHOASKED", "%s vroeg ons de definitie te vinden voor: ");
+define("_MD_IMGLOSSARY_WHOSUBMITTED", "%s heeft de definitie ingezonden voor deze term: ");
+define("_MD_IMGLOSSARY_YES", "Ja");
 
-define("_MD_IMGLOSSARY_OPTIONS", "Options");
-define("_MD_IMGLOSSARY_DOHTML", "Allow HTML?");
-define("_MD_IMGLOSSARY_DOSMILEY", "Allow smilies?");
-define("_MD_IMGLOSSARY_DOXCODE", "Allow ImpressCMS Codes?");
-define("_MD_IMGLOSSARY_BREAKS", "Allow linebreaks?");
-define("_MD_IMGLOSSARY_SUBANDREQ", "Submitted and requested terms");
-define("_MD_IMGLOSSARY_SUB", "Submitted terms: ");
-define("_MD_IMGLOSSARY_REQ", "Requested terms: ");
-define("_MD_IMGLOSSARY_NOSUB", "Currently there are no submitted terms.");
-define("_MD_IMGLOSSARY_NOREQ", "Currently there are no requested terms.");
-define("_MD_IMGLOSSARY_WRITEHERE", "Please write here the definition.");
-define("_MD_IMGLOSSARY_GUEST","Guest");
-define("_MD_IMGLOSSARY_NOCATSINSYSTEM","No categories in system");
-define("_MD_IMGLOSSARY_REQUESTSUGGEST","(Click the next words to suggest a definition for the term)");
+define("_MD_IMGLOSSARY_OPTIONS", "Opties");
+define("_MD_IMGLOSSARY_DOHTML", "Gebruik HTML?");
+define("_MD_IMGLOSSARY_DOSMILEY", "Gebruik smilies?");
+define("_MD_IMGLOSSARY_DOXCODE", "Gebruik ImpressCMS codes?");
+define("_MD_IMGLOSSARY_BREAKS", "Gebruik linebreak conversie?");
+define("_MD_IMGLOSSARY_SUBANDREQ", "Ingezonden en aangevraagde termen");
+define("_MD_IMGLOSSARY_SUB", "Ingezonden termen: ");
+define("_MD_IMGLOSSARY_REQ", "Aangevraagde termen: ");
+define("_MD_IMGLOSSARY_NOSUB", "Momenteel zijn er geen ingezonden termen.");
+define("_MD_IMGLOSSARY_NOREQ", "Momenteel zijn er geen aangevraagde termen.");
+define("_MD_IMGLOSSARY_WRITEHERE", "Schrijf hier een definitie.");
+define("_MD_IMGLOSSARY_GUEST","Gast");
+define("_MD_IMGLOSSARY_NOCATSINSYSTEM","Geen categorie in systeem");
+define("_MD_IMGLOSSARY_REQUESTSUGGEST","(Klik op de woorden om een defintie voor de term in te zenden)");
 
 // imGlossary v1.00
-define("_MD_IMGLOSSARY_ADMININDEX", "Administration");
-define("_MD_IMGLOSSARY_SUBMITDATE", "Submitted on %s. | ");
-define("_MD_IMGLOSSARY_ADDTO", "Add to ");
+define("_MD_IMGLOSSARY_ADMININDEX", "Administratie");
+define("_MD_IMGLOSSARY_SUBMITDATE", "Ingezonden op %s. | ");
+define("_MD_IMGLOSSARY_ADDTO", "Toevoegen aan ");
 ?>
--- language/nederlands/modinfo.php
+++ language/nederlands/modinfo.php
@@ -1,91 +1,93 @@
 <?php
 /**
- * $Id: main.php v 1.0 8 May 2004 hsalazar Exp $
- * Module: Wordbook - a multicategory glossary
- * Version: v 1.00
- * Release Date: 8 May 2004
- * Author: hsalazar
+ * $Id: modinfo.php 
+ * Module: imGlossary - a multicategory glossary
+ * Author: McDonald
+ * Language: Dutch (Nederlands)
  * Licence: GNU
  */
+ 
+global $xoopsModule;
 
 // Module Info
 // The name of this module
-global $xoopsModule;
+
 define("_MI_IMGLOSSARY_MD_NAME", "imGlossary");
 
 // A brief description of this module
-define("_MI_IMGLOSSARY_MD_DESC", "A multicategory glossary");
+define("_MI_IMGLOSSARY_MD_DESC", "Een multi-categorie verklarende woordenlijst");
 
 // Sub menus in main menu block
-define("_MI_IMGLOSSARY_SUB_SMNAME1", "Submit an entry");
-define("_MI_IMGLOSSARY_SUB_SMNAME2", "Request a definition");
-define("_MI_IMGLOSSARY_SUB_SMNAME3", "Search for a definition");
+define("_MI_IMGLOSSARY_SUB_SMNAME1", "Inzenden term");
+define("_MI_IMGLOSSARY_SUB_SMNAME2", "Aanvragen term");
+define("_MI_IMGLOSSARY_SUB_SMNAME3", "Zoek een definitie");
 
-define("_MI_IMGLOSSARY_RANDOMTERM", "Random term");
+define("_MI_IMGLOSSARY_RANDOMTERM", "Willekeurige term");
 
 // A brief description of this module
-define("_MI_IMGLOSSARY_ALLOWSUBMIT", "Can users submit entries?");
-define("_MI_IMGLOSSARY_ALLOWSUBMITDSC", "If set to <em>Yes</em>, users will have access to a submission form");
+define("_MI_IMGLOSSARY_ALLOWSUBMIT", "Kunnen bezoekers termen inzenden?");
+define("_MI_IMGLOSSARY_ALLOWSUBMITDSC", "Indien <em>Ja</em>, dan hebben bezoekers toegang tot het inzendformulier");
 
-define("_MI_IMGLOSSARY_ANONSUBMIT", "Can guests submit entries?");
-define("_MI_IMGLOSSARY_ANONSUBMITDSC", "If set to <em>Yes</em>, guests will have access to a submission form");
+define("_MI_IMGLOSSARY_ANONSUBMIT", "Kunnen gasten termen inzenden?");
+define("_MI_IMGLOSSARY_ANONSUBMITDSC", "Indien <em>Ja</em>, dan hebben gasten toegang tot het inzendformulier");
 
-define("_MI_IMGLOSSARY_DATEFORMAT", "In what format should the date appear?");
-define("_MI_IMGLOSSARY_DATEFORMATDSC", "Use the final part of language/english/global.php to select a display style.<br />See <a href='http://jp.php.net/manual/en/function.date.php' target='_blank'>PHP manual</a>");
+define("_MI_IMGLOSSARY_DATEFORMAT", "Weergave formaat voor de datum?");
+define("_MI_IMGLOSSARY_DATEFORMATDSC", "Voor informatie <a href='http://jp.php.net/manual/en/function.date.php' target='_blank'>PHP handleiding</a>");
+//define("_MI_IMGLOSSARY_DATEFORMATDSC", "Gebruik het deel <em>TIME FORMAT SETTINGS</em> van language/nederlands/global.php om een weergave stijl in te stellen.<br />Voor meer informatie <a href='http://jp.php.net/manual/en/function.date.php' target='_blank'>PHP handleiding</a>");
 
-define("_MI_IMGLOSSARY_PERPAGE", "Number of entries per page (Admin side)?");
-define("_MI_IMGLOSSARY_PERPAGEDSC", "Number of entries that will be shown at once in the table that displays active entries in the admin side.");
+define("_MI_IMGLOSSARY_PERPAGE", "Aantal termen per pagina (Admin zijde)?");
+define("_MI_IMGLOSSARY_PERPAGEDSC", "Aantal termen die per pagina worden getoond in de administratie van de module.<br />Standaard: <em>5</em>");
 
-define("_MI_IMGLOSSARY_PERPAGEINDEX", "Number of entries per page (User side)?");
-define("_MI_IMGLOSSARY_PERPAGEINDEXDSC", "Number of entries that will be shown on each page in the user side of the module.");
+define("_MI_IMGLOSSARY_PERPAGEINDEX", "Aantal termen per pagina (Gebruikers zijde)?");
+define("_MI_IMGLOSSARY_PERPAGEINDEXDSC", "Aantal termen die per pagina worden getoond aan de gebruikers kant van de module.<br />Standaard: <em>5</em>");
 
-define("_MI_IMGLOSSARY_AUTOAPPROVE", "Approve entries automatically?");
-define("_MI_IMGLOSSARY_AUTOAPPROVEDSC", "If set to <em>Yes</em>, ImpressCMS will publish submitted entries without admin intervention.");
+define("_MI_IMGLOSSARY_AUTOAPPROVE", "Termen automatisch goedkeuren?");
+define("_MI_IMGLOSSARY_AUTOAPPROVEDSC", "Indien <em>Ja</em>, dan publiceerd ImpressCMS ingezonden termen automatisch zonder tussenkomst van de websitebeheerder.");
 
-define("_MI_IMGLOSSARY_MULTICATS", "Do you want to have glossary categories?");
-define("_MI_IMGLOSSARY_MULTICATSDSC", "If set to <em>Yes</em>, will allow you to have glossary categories. If set to no, will have a single automatic category.");
+define("_MI_IMGLOSSARY_MULTICATS", "Wilt u categorieën toepassen?");
+define("_MI_IMGLOSSARY_MULTICATSDSC", "Indien <em>Ja</em>, dan kunt u categorieën aanmaken. Indien <em>Nee</em> zal één automatische categorie toegepast worden.");
 
-define("_MI_IMGLOSSARY_CATSINMENU","Should the categories be shown in the menu?"); 
-define("_MI_IMGLOSSARY_CATSINMENUDSC","If set to <em>Yes</em> if you want links to categories in the main menu."); 
+define("_MI_IMGLOSSARY_CATSINMENU", "Categorieën in het hoofdmenu weergegeven?"); 
+define("_MI_IMGLOSSARY_CATSINMENUDSC", "Indien <em>Ja</em>, dan worden de categorieën in het hoofdmenu weergegeven."); 
 
-define("_MI_IMGLOSSARY_CATSPERINDEX","Number of categories per page (User side)?"); 
-define("_MI_IMGLOSSARY_CATSPERINDEXDSC","This will define how many categories will be shown in the index page."); 
+define("_MI_IMGLOSSARY_CATSPERINDEX", "Aantal categorieën per pagina (Gebruikers zijde)?"); 
+define("_MI_IMGLOSSARY_CATSPERINDEXDSC", "Dit geeft aan hoeveel categorieën per pagina worden weergegeven.<br />Standaard: <em>5</em>"); 
 
-define("_MI_IMGLOSSARY_ALLOWADMINHITS", "Will the admin hits be included in the counter?");
-define("_MI_IMGLOSSARY_ALLOWADMINHITSDSC", "If set to <em>Yes</em>, will increase counter for each entry on admin visits.");
+define("_MI_IMGLOSSARY_ALLOWADMINHITS", "Dienen de admin hits meegetelt te worden?");
+define("_MI_IMGLOSSARY_ALLOWADMINHITSDSC", "Indien <em>Ja</em>, dan zal de teller verhoogd worden iedere keer als de admin de tterm bekijkt.");
 
-define("_MI_IMGLOSSARY_MAILTOADMIN", "Send mail to admin on each new submission?");  
-define("_MI_IMGLOSSARY_MAILTOADMINDSC", "If set to <em>Yes</em>, the manager will receive an e-mail for every submitted entry."); 
+define("_MI_IMGLOSSARY_MAILTOADMIN", "Stuur een email naar de admin bij iedere nieuwe inzending?");  
+define("_MI_IMGLOSSARY_MAILTOADMINDSC", "Indien <em>Ja</em>, dan zal de admin een email ontvangen voor iedere ingezonden term."); 
  
-define("_MI_IMGLOSSARY_RANDOMLENGTH", "Length of string to show in random definitions?");  
-define("_MI_IMGLOSSARY_RANDOMLENGTHDSC", "How many characters do you want to show in the random term boxes, both in the main page and in the block? (Default: 500)");
+define("_MI_IMGLOSSARY_RANDOMLENGTH", "Lengte van de tekst voor <em>willekeurige definities</em>?");  
+define("_MI_IMGLOSSARY_RANDOMLENGTHDSC", "Vul hier het aantal karakters in voor de willekeurige term blokken?<br />Standaard: 500");
 
-define("_MI_IMGLOSSARY_LINKTERMS", "Show links to other glossary terms in the definitions?");  
-define("_MI_IMGLOSSARY_LINKTERMSDSC", "If set to <em>Yes</em>, imGlossary will automatically link in your definitions those terms you already have in your glossaries.");
+define("_MI_IMGLOSSARY_LINKTERMS", "Laat linken naar andere termen zien in de definities?");  
+define("_MI_IMGLOSSARY_LINKTERMSDSC", "Indien <em>Ja</em>, zal imGlossary automatisch linken in uw definities naar andere termen in de woordenlijsten.");
 
 // Names of admin menu items
 define("_MI_IMGLOSSARY_ADMENU1", "Index");
-define("_MI_IMGLOSSARY_ADMENU2", "Add category");
-define("_MI_IMGLOSSARY_ADMENU3", "Add entry");
-define("_MI_IMGLOSSARY_ADMENU4", "Blocks");
-define("_MI_IMGLOSSARY_ADMENU5", "Go to module");
+define("_MI_IMGLOSSARY_ADMENU2", "Nieuwe categorie");
+define("_MI_IMGLOSSARY_ADMENU3", "Nieuwe term");
+define("_MI_IMGLOSSARY_ADMENU4", "Blokken");
+define("_MI_IMGLOSSARY_ADMENU5", "Naar module");
 //mondarse
-define("_MI_IMGLOSSARY_ADMENU6", "Import");
+define("_MI_IMGLOSSARY_ADMENU6", "Importeer");
 
 //Names of Blocks and Block information
-define("_MI_IMGLOSSARY_ENTRIESNEW", "Newest Terms");
-define("_MI_IMGLOSSARY_ENTRIESTOP", "Most Read Terms");
+define("_MI_IMGLOSSARY_ENTRIESNEW", "Nieuwste termen");
+define("_MI_IMGLOSSARY_ENTRIESTOP", "Meest gelezen termen");
 
 // imGlossary - version 1.00
-define("_MI_IMGLOSSARY_SORTCATS", "Sort categories by:");
-define("_MI_IMGLOSSARY_SORTCATSDSC", "Select how categories are sorted.");
-define("_MI_IMGLOSSARY_TITLE", "Title");
-define("_MI_IMGLOSSARY_WEIGHT", "Weight");
-define("_MI_IMGLOSSARY_ADMENU7", "Submissions");
-define("_MI_IMGLOSSARY_SHOWSUBMITTER", "Show submitter in every entry?");
-define("_MI_IMGLOSSARY_SHOWSUBMITTERDSC", "Select <em>Yes</em> to display the submitter of the entry.");
-define("_MI_IMGLOSSARY_SHOWSBOOKMARKS","Show Social Bookmarks in every entry?");
-define("_MI_IMGLOSSARY_SHOWSBOOKMARKSDSC", "Select <em>Yes</em> to have Social Bookmark icons visible in every entry.");
-define("_MI_IMGLOSSARY_SEARCHCOLOR", "Background colour for search terms:");
-define("_MI_IMGLOSSARY_SEARCHCOLORDSC", "Enter the colour to use as background for search terms. Default: <span style='background-color: #FFFF00;'>&nbsp;#FFFF00&nbsp;</span>");
+define("_MI_IMGLOSSARY_SORTCATS", "Sorteer categorieën op:");
+define("_MI_IMGLOSSARY_SORTCATSDSC", "Selekteer hoe categorieën gesorteerd worden.");
+define("_MI_IMGLOSSARY_TITLE", "Titel");
+define("_MI_IMGLOSSARY_WEIGHT", "Gewicht");
+define("_MI_IMGLOSSARY_ADMENU7", "Inzendingen");
+define("_MI_IMGLOSSARY_SHOWSUBMITTER", "Laat inzender voor iedere term zien?");
+define("_MI_IMGLOSSARY_SHOWSUBMITTERDSC", "Kies <em>Ja</em> om de inzender van de term weer te geven.");
+define("_MI_IMGLOSSARY_SHOWSBOOKMARKS","Laat Social Bookmarks zien bij iedere term?");
+define("_MI_IMGLOSSARY_SHOWSBOOKMARKSDSC", "Kies <em>Ja</em> om de Social Bookmark iconen weer te geven bij iedere term.");
+define("_MI_IMGLOSSARY_SEARCHCOLOR", "Achtergrond kleur voor zoektermen:");
+define("_MI_IMGLOSSARY_SEARCHCOLORDSC", "Voer de kleur in die als achtergroind dient voor zoektermen.<br />Standaard: <span style='background-color: #FFFF00; font-style: italic;'>&nbsp;#FFFF00&nbsp;</span>");
 ?>
