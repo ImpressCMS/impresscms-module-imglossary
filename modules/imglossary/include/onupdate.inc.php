a5504fae25f19bd3c702378aeb53125c1623e0b6
Ronald <pietjebell31@hotmail.com>
2008-10-22 23:29:43 +0000

    Fix for block settings [skenow]
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imglossary/trunk@5900 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- modules/imglossary/include/onupdate.inc.php
+++ modules/imglossary/include/onupdate.inc.php
@@ -4,20 +4,16 @@
  * Module: imGlossary
  */
 
-if( ! defined( 'ICMS_ROOT_PATH' ) ) exit ;
+if ( ! defined( 'ICMS_ROOT_PATH' ) ) exit;
 
 // referer check
 $ref = xoops_getenv('HTTP_REFERER');
-if( $ref == '' || strpos( $ref , ICMS_URL.'/modules/system/admin.php' ) === 0 ) {
+if ( $ref == '' || strpos( $ref, ICMS_URL . '/modules/system/admin.php' ) === 0 ) {
 	/* module specific part */
-
-
 
 	/* General part */
 
 	// Keep the values of block's options when module is updated (by nobunobu)
-	include dirname( __FILE__ ) . "/updateblock.inc.php" ;
-
+	include dirname( __FILE__ ) . "/updateblock.inc.php";
 }
-
 ?>
