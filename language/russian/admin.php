6ae96f3d57c552f58c0e72447861a0557e6bd947
algalochkin <contact@impresscms.org>
2008-10-31 17:01:11 +0000

    Russian translation added
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imglossary/trunk/modules/imglossary@6287 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ language/russian/admin.php
@@ -0,0 +1,163 @@
+<?php
+/**
+* imGlossary - a multicategory glossary for ImpressCMS
+*
+* Based upon Wordbook 1.16
+*
+* File: language/english/admin.php
+*
+* @copyright		http://www.xoops.org/ The XOOPS Project
+* @copyright		XOOPS_copyrights.txt
+* @copyright		http://www.impresscms.org/ The ImpressCMS Project
+* @license		GNU General Public License (GPL)
+*				a copy of the GNU license is enclosed.
+* ----------------------------------------------------------------------------------------------------------
+* @package		Wordbook - a multicategory glossary
+* @since			1.16
+* @author		hsalazar
+* ----------------------------------------------------------------------------------------------------------
+* 				imGlossary - a multicategory glossary
+* @since			1.00
+* @author		modified by McDonald
+* @version		$Id$ Russian translation. Charset: utf-8 (without BOM)
+*/
+ 
+define("_AM_IMGLOSSARY_ACTION", "Действие");
+define("_AM_IMGLOSSARY_ADMINCATMNGMT", "Редактирование категории");
+define("_AM_IMGLOSSARY_ADMINENTRYMNGMT", "Редактирование записи");
+define("_AM_IMGLOSSARY_ALLOWCOMMENTS", "Можно комментировать запись?");
+define("_AM_IMGLOSSARY_ANON", "Гость");
+define("_AM_IMGLOSSARY_AUTHENTRY", "Разрешить подчинение");
+define("_AM_IMGLOSSARY_AUTHOR", "Автор");
+define("_AM_IMGLOSSARY_AUTHORIZE", "Разрешить");
+define("_AM_IMGLOSSARY_BACK2IDX", "Отменено. Возвращамся обратно к списку");
+define("_AM_IMGLOSSARY_BLOCK", " Добавить в блок записей?");
+define("_AM_IMGLOSSARY_BLOCKS", "Блоки");
+define("_AM_IMGLOSSARY_BREAKS", " Использовать конверсию перевода строки?");
+define("_AM_IMGLOSSARY_CANCEL", "Отменить");
+define("_AM_IMGLOSSARY_CATCREATED", "Новая категория создана и сохранена!");
+define("_AM_IMGLOSSARY_CATDESCRIPT", "Описание категории");
+define("_AM_IMGLOSSARY_CATIMAGE", "Изображение категории");
+define("_AM_IMGLOSSARY_CATIMAGEUPLOAD", " Загрузить изображение категории");
+define("_AM_IMGLOSSARY_CATISDELETED", "Категория %s удалена");
+define("_AM_IMGLOSSARY_CATMODIFIED", "Выбранная категория отредактирована и сохранена!");
+define("_AM_IMGLOSSARY_CATNAME", "Наименование категории");
+define("_AM_IMGLOSSARY_CATPOSIT", "Позиция категории");
+define("_AM_IMGLOSSARY_CATS", "Категории");
+define("_AM_IMGLOSSARY_CATSHEADER", "Редактирование категории");
+define("_AM_IMGLOSSARY_CLEAR", "Очистить");
+define("_AM_IMGLOSSARY_CREATE", "Создать");
+define("_AM_IMGLOSSARY_CREATECAT", "Добавить категорию");
+define("_AM_IMGLOSSARY_CREATEENTRY", "Добавить запись");
+define("_AM_IMGLOSSARY_CREATEIN", "Создать в категории:");
+define("_AM_IMGLOSSARY_DELETE", "Удалить");
+define("_AM_IMGLOSSARY_DELETECAT", "Удалить категорию");
+define("_AM_IMGLOSSARY_DELETEENTRY", "Удалить запись");
+define("_AM_IMGLOSSARY_DELETESUBM", "Удалить подчиненные");
+define("_AM_IMGLOSSARY_DELETETHISCAT", "Вы желаете удалить эту категорию?");
+define("_AM_IMGLOSSARY_DELETETHISENTRY", "Удалить эту запись?");
+define("_AM_IMGLOSSARY_DESCRIP", "Описание категории");
+define("_AM_IMGLOSSARY_DOHTML", " Разрешить HTML тэги");
+define("_AM_IMGLOSSARY_DOSMILEY", " Разрешить смайлики");
+define("_AM_IMGLOSSARY_DOXCODE", " Разрешить iCMS коды");
+define("_AM_IMGLOSSARY_EDITCAT", "Редактирование категории");
+define("_AM_IMGLOSSARY_EDITENTRY", "Редактирование записи");
+define("_AM_IMGLOSSARY_EDITSUBM", "Редактирование размещения");
+define("_AM_IMGLOSSARY_ENTRIES", "Записи");
+define("_AM_IMGLOSSARY_ENTRYAUTHORIZED", "Запись авторизована.");
+define("_AM_IMGLOSSARY_ENTRYCATNAME", "Наименование категории");
+define("_AM_IMGLOSSARY_ENTRYCREATED", "Создано");
+define("_AM_IMGLOSSARY_ENTRYCREATEDOK", "Запись создана!");
+define("_AM_IMGLOSSARY_ENTRYDEF", "Определение");
+define("_AM_IMGLOSSARY_ENTRYID", "Id");
+define("_AM_IMGLOSSARY_ENTRYISDELETED", "Запись %s удалена.");
+define("_AM_IMGLOSSARY_ENTRYISOFF", "Запись отключена");
+define("_AM_IMGLOSSARY_ENTRYISON", "Запись подключена");
+define("_AM_IMGLOSSARY_ENTRYMODIFIED", "Запись отредактирована!");
+define("_AM_IMGLOSSARY_ENTRYNOTCREATED", "Извините. It was not possible to create this entry!");
+define("_AM_IMGLOSSARY_ENTRYNOTUPDATED", "Извините. It was not possible to update the entry!");
+define("_AM_IMGLOSSARY_ENTRYREFERENCE", "Reference<span style='font-size: xx-small; font-weight: normal; display: block;'>(Write here the source of your<br />definition, such as a book,<br />article, or person.)</span>");
+define("_AM_IMGLOSSARY_ENTRYTERM", "Термин");
+define("_AM_IMGLOSSARY_ENTRYURL", "Связанный сайт<span style='font-size: xx-small; font-weight: normal; display: block;'>(Please enter a valid URL with<br />or without the HTTP prefix.)</span>");
+define("_AM_IMGLOSSARY_FILEEXISTS", "A file with that name already exists on server. Please choose another one!");
+define("_AM_IMGLOSSARY_GOMOD", "Перейти в модуль");
+define("_AM_IMGLOSSARY_HELP", "Помощь");
+define("_AM_IMGLOSSARY_ID", "Id");
+define("_AM_IMGLOSSARY_INDEX", "Индекс");
+define("_AM_IMGLOSSARY_INVENTORY", "Резюме");
+define("_AM_IMGLOSSARY_MODCAT", "Редактирование существующей категории");
+define("_AM_IMGLOSSARY_MODADMIN", " Module Admin: ");
+define("_AM_IMGLOSSARY_MODENTRY", "Редактирование записи");
+define("_AM_IMGLOSSARY_MODIFY", "Редактирование");
+define("_AM_IMGLOSSARY_MODIFYCAT", "Редактирование категории");
+define("_AM_IMGLOSSARY_MODIFYTHISCAT", "Редактировать эту категорию?");
+define("_AM_IMGLOSSARY_MODULEHEADMULTI", "Записи | Категории | Размещения | Запросы");
+define("_AM_IMGLOSSARY_MODULEHEADSINGLE", "Записи | Размещения | Запросы");
+define("_AM_IMGLOSSARY_NEEDONECOLUMN", "Для ввода записи Вам необходимо иметь хотя бы одну категорию.");
+define("_AM_IMGLOSSARY_NEWCAT", "Создание категории");
+define("_AM_IMGLOSSARY_NEWENTRY", "Создание записи");
+define("_AM_IMGLOSSARY_NO", "Нет");
+define("_AM_IMGLOSSARY_NOCATS", "Нет категорий для показа");
+define("_AM_IMGLOSSARY_NOCOLTOEDIT", "Отсутствуют колонки для редактирования!");
+define("_AM_IMGLOSSARY_NOPERMSSET", "Невозможно установить права доступа: Колонки еще не созданы!");
+define("_AM_IMGLOSSARY_NOREQSYET", "Нет запрошенных терминов.");
+define("_AM_IMGLOSSARY_NOSUBMISSYET", "Нет размещений, ожидающих утверждения.");
+define("_AM_IMGLOSSARY_NOTERMS", "Нет терминов для отображения");
+define("_AM_IMGLOSSARY_NOTUPDATED", "Произошла ошибка во время обновления базы данных!");
+define("_AM_IMGLOSSARY_OPTIONS", "Опции");
+define("_AM_IMGLOSSARY_OPTS", "Предпочтения");
+define("_AM_IMGLOSSARY_SHOWCATS", "Категории");
+define("_AM_IMGLOSSARY_SHOWENTRIES", "Записи");
+define("_AM_IMGLOSSARY_SHOWREQUESTS", "Запросы");
+define("_AM_IMGLOSSARY_SHOWSUBMISSIONS", "Размещения");
+define("_AM_IMGLOSSARY_SINGLECAT", "The module is defined to have a single category. You have no reason to be here.");
+define("_AM_IMGLOSSARY_STATUS", "Статус");
+define("_AM_IMGLOSSARY_SUBMITS", "Размещения");
+define("_AM_IMGLOSSARY_SUBMITTER", "Кто разместил");
+define("_AM_IMGLOSSARY_SWITCHOFFLINE", " Перевести запись в автономный режим?");
+define("_AM_IMGLOSSARY_TOTALCATS", "Доступно категорий: ");
+define("_AM_IMGLOSSARY_TOTALENTRIES", "Опубликовано записей: ");
+define("_AM_IMGLOSSARY_TOTALREQ", "Запрошено записей: ");
+define("_AM_IMGLOSSARY_TOTALSUBM", "Размещено записей: ");
+define("_AM_IMGLOSSARY_UNIQUE", "Уникальная категория");
+define("_AM_IMGLOSSARY_WEIGHT", "Вес");
+define("_AM_IMGLOSSARY_WRITEHERE", "Напишите здесь определение, пожалуйста.");
+define("_AM_IMGLOSSARY_YES", "Да");
+//mondarse
+define("_AM_IMGLOSSARY_IMPORT", "Импорт");
+define("_AM_IMGLOSSARY_IMPORTWARN", "Предупреждение!! Прежде чем продолжить, сохраните Вашу базу данных.<br />Версия скрипта импорта имеет статус Альфа, и это может привести к порче данных.");
+
+// Taken from importdictionary091.php (McDonald)
+define("_AM_IMGLOSSARY_IMPDICT_01","База данных для импорта отсутствует или пуста!");
+define("_AM_IMGLOSSARY_IMPDICT_02","imGlossary Entries Import Script");
+define("_AM_IMGLOSSARY_IMPDICT_03","Импортировать из Dictionary вер. 0.92");
+define("_AM_IMGLOSSARY_IMPDICT_04","Ошибка: #");
+define("_AM_IMGLOSSARY_IMPDICT_05","Dictionary ID: ");
+define("_AM_IMGLOSSARY_IMPDICT_06","imGlossary ID: ");
+define("_AM_IMGLOSSARY_IMPDICT_07","Error while moving Comments from Dictionary to imGlossary module.");
+define("_AM_IMGLOSSARY_IMPDICT_08","Comments successfully moved from Dictionary to imGlossary.");
+define("_AM_IMGLOSSARY_IMPDICT_09","Incorrectly: ");
+define("_AM_IMGLOSSARY_IMPDICT_10","Processed: ");
+define("_AM_IMGLOSSARY_IMPDICT_11","Вернуться к администрированию");
+define("_AM_IMGLOSSARY_IMPDICT_12","Модуль Dictionary не найден на этом сайте.");
+
+define("_AM_IMGLOSSARY_NOCOLEXISTS", "Извините, there are no categories defined as of yet.<br />Please contact the site administrator and tell him about this."); // This was originally in the main.php file
+
+// imGlossary v1.00
+define("_AM_IMGLOSSARY_SHOWOFFLINE", "Автономный режим");
+define("_AM_IMGLOSSARY_COMMENTS", "Комментарии");
+define("_AM_IMGLOSSARY_ABOUT", "О модуле");
+define("_AM_IMGLOSSARY_UPDATEMOD", "Обновить модуль");
+
+// Import 
+define("_AM_IMGLOSSARY_MODULEHEADIMPORTWB", "Wordbook &mdash;&gt; imGlossary import script");
+define("_AM_IMGLOSSARY_MODULEHEADIMPORT", "Dictionary 0.91 &mdash;&gt; imGlossary import script");
+define("_AM_IMGLOSSARY_MODULEHEADIMPORTGLO", "Glossaire &mdash;&gt; imGlossary import script");
+define("_AM_IMGLOSSARY_MODULEHEADIMPORTXWO", "XWords &mdash;&gt; imGlossary import script");
+define("_AM_IMGLOSSARY_MODULEHEADIMPORTWW", "Wiwimod &mdash;&gt; imGlossary import script");
+define("_AM_IMGLOSSARY_MODULEIMPORTEMPTY10", "База данных отсутствует или пуста!");
+define("_AM_IMGLOSSARY_MODULEIMPORTERCOM", "Комментарии перемещены в imGlossary");
+define("_AM_IMGLOSSARY_MODULEIMPORTERNOCOM", "Ошибкаво время перемещения Комментариев в imGlossary");
+define("_AM_IMGLOSSARY_IMPORTDELWB","Удалить записи перед импортом? ");
+define("_AM_IMGLOSSARY_NOOTHERMODS", "No corresponding modules could be located!");
+?>
---
+++ language/russian/modinfo.php
@@ -0,0 +1,123 @@
+<?php
+/**
+* imGlossary - a multicategory glossary for ImpressCMS
+*
+* Based upon Wordbook 1.16
+*
+* File: language/english/modinfo.php
+*
+* @copyright		http://www.xoops.org/ The XOOPS Project
+* @copyright		XOOPS_copyrights.txt
+* @copyright		http://www.impresscms.org/ The ImpressCMS Project
+* @license		GNU General Public License (GPL)
+*				a copy of the GNU license is enclosed.
+* ----------------------------------------------------------------------------------------------------------
+* @package		Wordbook - a multicategory glossary
+* @since			1.16
+* @author		hsalazar
+* ----------------------------------------------------------------------------------------------------------
+* 				imGlossary - a multicategory glossary
+* @since			1.00
+* @author		modified by McDonald
+* @version		$Id$ Russian translation. Charset: utf-8 (without BOM)
+*/
+
+// The name of this module
+global $xoopsModule;
+define("_MI_IMGLOSSARY_MD_NAME", "imGlossary");
+
+// A brief description of this module
+define("_MI_IMGLOSSARY_MD_DESC", "Многокатегорийный глоссарий (словарь)");
+
+// Sub menus in main menu block
+define("_MI_IMGLOSSARY_SUB_SMNAME1", "Размещение записи");
+define("_MI_IMGLOSSARY_SUB_SMNAME2", "Запрос определения");
+define("_MI_IMGLOSSARY_SUB_SMNAME3", "Поиск определения");
+
+define("_MI_IMGLOSSARY_RANDOMTERM", "Случайный термин");
+
+// A brief description of this module
+define("_MI_IMGLOSSARY_ALLOWSUBMIT", "Пользователи могут размещать записи?");
+define("_MI_IMGLOSSARY_ALLOWSUBMITDSC", "Если установлено <em>Да</em>, то пользователи будут иметь доступ к заполнению формы размещения");
+
+define("_MI_IMGLOSSARY_ANONSUBMIT", "Гости могут размещать записи?");
+define("_MI_IMGLOSSARY_ANONSUBMITDSC", "Если установлено <em>Да</em>, то гости будут иметь доступ к заполнению формы размещения");
+
+define("_MI_IMGLOSSARY_DATEFORMAT", "В каком формате должна быть представлена дата?");
+define("_MI_IMGLOSSARY_DATEFORMATDSC", "Use the part <em>TIME FORMAT SETTINGS</em> of language/english/global.php to select a display style.<br />See <a href='http://jp.php.net/manual/en/function.date.php' target='_blank'>PHP manual</a>");
+
+define("_MI_IMGLOSSARY_PERPAGE", "Кол-во записей на странице (для администратора)?");
+define("_MI_IMGLOSSARY_PERPAGEDSC", "Number of entries that will be shown at once in the table that displays active entries in the admin side.");
+
+define("_MI_IMGLOSSARY_PERPAGEINDEX", "Кол-во записей на странице (для пользователя)?");
+define("_MI_IMGLOSSARY_PERPAGEINDEXDSC", "Number of entries that will be shown on each page in the user side of the module.");
+
+define("_MI_IMGLOSSARY_AUTOAPPROVE", "Утверждать записи автоматически?");
+define("_MI_IMGLOSSARY_AUTOAPPROVEDSC", "Если установлено <em>Да</em>, ImpressCMS will publish submitted entries without admin intervention.");
+
+define("_MI_IMGLOSSARY_MULTICATS", "Do you want to have glossary categories?");
+define("_MI_IMGLOSSARY_MULTICATSDSC", "Если установлено <em>Да</em>, will allow you to have glossary categories. Если установлено <em>Нет</em>, will have a single automatic category.");
+
+define("_MI_IMGLOSSARY_CATSINMENU","Should the categories be shown in the menu?"); 
+define("_MI_IMGLOSSARY_CATSINMENUDSC","Если установлено <em>Да</em> if you want links to categories in the main menu."); 
+
+define("_MI_IMGLOSSARY_CATSPERINDEX","Кол-во категорий на странице (для пользователя)?"); 
+define("_MI_IMGLOSSARY_CATSPERINDEXDSC","This will define how many categories will be shown in the index page."); 
+
+define("_MI_IMGLOSSARY_ALLOWADMINHITS", "Will the admin hits be included in the counter?");
+define("_MI_IMGLOSSARY_ALLOWADMINHITSDSC", "Если установлено <em>Да</em>, will increase counter for each entry on admin visits.");
+
+define("_MI_IMGLOSSARY_MAILTOADMIN", "Отсылать email администратору о каждом новом размещении?");  
+define("_MI_IMGLOSSARY_MAILTOADMINDSC", "Если установлено <em>Да</em>, the manager will receive an e-mail for every submitted entry."); 
+ 
+define("_MI_IMGLOSSARY_RANDOMLENGTH", "Длина строки для показа в произвольных определениях?");  
+define("_MI_IMGLOSSARY_RANDOMLENGTHDSC", "Какое кол-во символов Вы желаете показывать в боксах случайных терминов, both in the main page and in the block? (По умолчанию: 500)");
+
+define("_MI_IMGLOSSARY_LINKTERMS", "Показывать ссылки на другие термины из глоссария в определениях?");  
+define("_MI_IMGLOSSARY_LINKTERMSDSC", "Если установлено <em>Да</em>, imGlossary будет автоматически ссылаться в Вашем определении на термины, которые уже имеются в глоссариях.");
+
+// Names of admin menu items
+define("_MI_IMGLOSSARY_ADMENU1", "Индекс");
+define("_MI_IMGLOSSARY_ADMENU2", "Добавить категорию");
+define("_MI_IMGLOSSARY_ADMENU3", "Добавить запись");
+define("_MI_IMGLOSSARY_ADMENU4", "Блоки");
+define("_MI_IMGLOSSARY_ADMENU5", "Перейти в модуль");
+//mondarse
+define("_MI_IMGLOSSARY_ADMENU6", "Импорт");
+
+//Names of Blocks and Block information
+define("_MI_IMGLOSSARY_ENTRIESNEW", "Новейшие термины");
+define("_MI_IMGLOSSARY_ENTRIESTOP", "Часто читаемые термины");
+
+// imGlossary - version 1.00
+define("_MI_IMGLOSSARY_SORTCATS", "Сортировать категории по:");
+define("_MI_IMGLOSSARY_SORTCATSDSC", "Select how categories are sorted.");
+define("_MI_IMGLOSSARY_TITLE", "Заголовок");
+define("_MI_IMGLOSSARY_WEIGHT", "Вес");
+define("_MI_IMGLOSSARY_ADMENU7", "Размещения");
+define("_MI_IMGLOSSARY_SHOWSUBMITTER", "Показывать разместившего в каждой записи?");
+define("_MI_IMGLOSSARY_SHOWSUBMITTERDSC", "Select <em>Yes</em> to display the submitter of the entry.");
+define("_MI_IMGLOSSARY_SHOWSBOOKMARKS","Show Social Bookmarks in every entry?");
+define("_MI_IMGLOSSARY_SHOWSBOOKMARKSDSC", "Select <em>Yes</em> to have Social Bookmark icons visible in every entry.");
+define("_MI_IMGLOSSARY_SEARCHCOLOR", "Цвет фона терминов при поиске:");
+define("_MI_IMGLOSSARY_SEARCHCOLORDSC", "Enter the colour to use as background for search terms. Default: <span style='background-color: #FFFF00;'>&nbsp;#FFFF00&nbsp;</span>");
+define("_MI_IMGLOSSARY_CAPTCHA", "Использовать captcha в формах размщения и запроса?");
+define("_MI_IMGLOSSARY_CAPTCHADSC", "Select <em>Yes</em> to use captcha in the submit and request form.<br />Default: <em>Yes</em>");
+define("_MI_IMGLOSSARY_SHOWCENTER", "Показать центральные блоки?");
+define("_MI_IMGLOSSARY_SHOWCENTERDSC", "Select <em>Yes</em> to display the three center blocks Recent entries, Popular entries and Search on the index page.<br />Select <em>No</em> to replace these 3 blocks by a Search block.");
+define("_MI_IMGLOSSARY_SHOWRANDOM", "Показать блок случайных терминов?");
+define("_MI_IMGLOSSARY_SHOWRANDOMDSC", "Select <em>Yes</em> to display the Random term block on the index page.");
+define('_MI_IMGLOSSARY_EDITORADMIN', "Использовать редактор (для администратора):");
+define('_MI_IMGLOSSARY_EDITORADMINDSC', "Select the editor to use for admin side.<br />In Preferences -> General Settings set 'Default Editor' to <em>dhtmltextarea</em>.");
+define('_MI_IMGLOSSARY_EDITORUSER', "Использовать редактор (для пользователя):");
+define('_MI_IMGLOSSARY_EDITORUSERDSC', "Select the editor to use for user side.<br />In Preferences -> General Settings set 'Default Editor' to <em>dhtmltextarea</em>.");
+define("_MI_IMGLOSSARY_FORM_DHTML", "DHTML");
+define("_MI_IMGLOSSARY_FORM_COMPACT", "Compact");
+define("_MI_IMGLOSSARY_FORM_HTMLAREA", "HtmlArea Editor");
+define("_MI_IMGLOSSARY_FORM_FCK", "FCK Editor");
+define("_MI_IMGLOSSARY_FORM_KOIVI", "Koivi Editor");
+define("_MI_IMGLOSSARY_FORM_INBETWEEN", "Inbetween");
+define("_MI_IMGLOSSARY_FORM_TINYEDITOR", "TinyEditor");
+define("_MI_IMGLOSSARY_FORM_TINYMCE", "TinyMCE");
+define("_MI_IMGLOSSARY_FORM_DHTMLEXT", "DHTML Extended");
+?>
