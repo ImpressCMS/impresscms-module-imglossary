504e9686b27c22a8765b17dd4c9d00eae8935fdb
algalochkin <contact@impresscms.org>
2008-10-31 17:01:11 +0000

    Russian translation added
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imglossary/trunk@6287 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ modules/imglossary/language/russian/blocks.php
@@ -0,0 +1,48 @@
+<?php
+/**
+* imGlossary - a multicategory glossary for ImpressCMS
+*
+* Based upon Wordbook 1.16
+*
+* File: language/english/blocks.php
+*
+* @copyright		http://www.xoops.org/ The XOOPS Project
+* @copyright		XOOPS_copyrights.txt
+* @copyright		http://www.impresscms.org/ The ImpressCMS Project
+* @license		GNU General Public License (GPL)
+*				a copy of the GNU license is enclosed.
+* ----------------------------------------------------------------------------------------------------------
+* @package		Wordbook - a multicategory glossary
+* @since			1.16
+* @author		hsalazar
+* ----------------------------------------------------------------------------------------------------------
+* 				imGlossary - a multicategory glossary
+* @since			1.00
+* @author		modified by McDonald
+* @version		$Id$ Russian translation. Charset: utf-8 (without BOM)
+*/
+
+// Blocks
+define("_MB_IMGLOSSARY_DATE", "Дата публикации");
+define("_MB_IMGLOSSARY_DELTERM", "Удалить этот период");
+define("_MB_IMGLOSSARY_DISP", "Показать");
+define("_MB_IMGLOSSARY_EDITTERM", "Редактировать этот период");
+define("_MB_IMGLOSSARY_HITS", "Кол-во обращений");
+define("_MB_IMGLOSSARY_INTENTRY", "Have a look at this entry at %s");
+define("_MB_IMGLOSSARY_INTENTRYFOUND", "Занятная информация, которую я нашел на %s");
+define("_MB_IMGLOSSARY_ORDER", "Порядок отображения");
+define("_MB_IMGLOSSARY_PRINTTERM", "Формат для печати");
+define("_MB_IMGLOSSARY_RANDOMTITLE", "Произвольный период");
+define("_MB_IMGLOSSARY_SEEMORE", "Подробнее...");
+define("_MB_IMGLOSSARY_SENDTOFRIEND", "Отправить этот term другу");
+define("_MB_IMGLOSSARY_TERMS", "периоды");
+define("_MB_IMGLOSSARY_TITLE", "Заголовок");
+define("_MB_IMGLOSSARY_WEIGHT", "Список по весу");
+
+// imGlossary v1.00
+define( '_MB_IMGLOSSARY_DATEFORMAT', 'Формат даты: ' );
+define( '_MB_IMGLOSSARY_DATEFORMATMANUAL', 'Смотрите <a href="http://jp.php.net/manual/en/function.date.php" target="_blank">PHP руководство</a>' );
+define( '_MB_IMGLOSSARY_CATEGORY', 'Категория: ' );
+define( '_MB_IMGLOSSARY_ADMININDEX', 'Администрирование' );
+define( '_MB_IMGLOSSARY_PDFTERM', 'Создать PDF');
+?>
---
+++ modules/imglossary/language/russian/main.php
@@ -0,0 +1,157 @@
+<?php
+/**
+* imGlossary - a multicategory glossary for ImpressCMS
+*
+* Based upon Wordbook 1.16
+*
+* File: language/english/main.php
+*
+* @copyright		http://www.xoops.org/ The XOOPS Project
+* @copyright		XOOPS_copyrights.txt
+* @copyright		http://www.impresscms.org/ The ImpressCMS Project
+* @license		GNU General Public License (GPL)
+*				a copy of the GNU license is enclosed.
+* ----------------------------------------------------------------------------------------------------------
+* @package		Wordbook - a multicategory glossary
+* @since			1.16
+* @author		hsalazar
+* ----------------------------------------------------------------------------------------------------------
+* 				imGlossary - a multicategory glossary
+* @since			1.00
+* @author		modified by McDonald
+* @version		$Id$ Russian translation. Charset: utf-8 (without BOM)
+*/
+
+define("_MD_IMGLOSSARY_NOCOLEXISTS", "Извините, Категории еще не определены.<br />Пожалуйста, свяжитесь с администратором сайта и сообщите его об этом.");
+define("_MD_IMGLOSSARY_ALL", "Все");
+define("_MD_IMGLOSSARY_ALLCATS", "Все категории");
+define("_MD_IMGLOSSARY_ALLOFTHEM", "0 : Все категории");
+define("_MD_IMGLOSSARY_ANDOR", "Search scope");
+define("_MD_IMGLOSSARY_ANDORALL", "Все слова (AND)");
+define("_MD_IMGLOSSARY_ANDORANY", "Любое из слов (OR)");
+define("_MD_IMGLOSSARY_ANDOREXACT", "Exact match");
+define("_MD_IMGLOSSARY_ANONYMOUS", "Анонимный");
+define("_MD_IMGLOSSARY_ASKEDBY", "Термин запрошен:");
+define("_MD_IMGLOSSARY_ASKFORDEF", "Запрос определения");
+define("_MD_IMGLOSSARY_BEGINWITHLETTER", "записей, котрые начинаются с этой буквы.");
+define("_MD_IMGLOSSARY_BROWSECAT", "По категориям");
+define("_MD_IMGLOSSARY_BROWSELETTER", "По начальным буквам");
+define("_MD_IMGLOSSARY_CATEGORY", "Категория");
+define("_MD_IMGLOSSARY_CATS", "Категории: ");
+define("_MD_IMGLOSSARY_COUNT", "Эта запись просмотрена %s раз.");
+define("_MD_IMGLOSSARY_CREATE", "Разместить запись");
+define("_MD_IMGLOSSARY_DEFINITION", "Определение");
+define("_MD_IMGLOSSARY_DEFINITIONREQ", "Запрос определения");
+define("_MD_IMGLOSSARY_DEFINITIONSUB", "Разместить определение");
+define("_MD_IMGLOSSARY_DEFINS", "Определения");
+define("_MD_IMGLOSSARY_DEFS", "Определения: ");
+define("_MD_IMGLOSSARY_DELTERM", "Удалить этот термин");
+define("_MD_IMGLOSSARY_EDITTERM", "Редактировать этот термин");
+define("_MD_IMGLOSSARY_EMAILLEFT", "Пользователь left этот email адрес: ");
+define("_MD_IMGLOSSARY_ENTRIES", " записей.");
+define("_MD_IMGLOSSARY_ENTRIESINCAT", "записей в этом глоссарии.");
+define("_MD_IMGLOSSARY_ENTRY", "Запись");
+define("_MD_IMGLOSSARY_ENTRYCATEGORY", "Категория: ");
+define("_MD_IMGLOSSARY_ENTRYCOUNT", "В этой категории ");
+define("_MD_IMGLOSSARY_ENTRYDEFINITION", "Определение: ");
+define("_MD_IMGLOSSARY_ENTRYREFERENCE", "Сноска: ");
+define("_MD_IMGLOSSARY_ENTRYRELATEDURL", "Связанный сайт: ");
+define("_MD_IMGLOSSARY_ERRORSAVINGDB", "ОШИБКА: База данных НЕ ОБНОВЛЕНА из-за ошибки!");
+define("_MD_IMGLOSSARY_GOODDAY", "Доброго дня, ");
+define("_MD_IMGLOSSARY_GOODDAY2", "Доброго дня, %s.");
+define("_MD_IMGLOSSARY_HOME", "Главная");
+define("_MD_IMGLOSSARY_INALLGLOSSARIES", "записей во всех Ваших глоссариях.");
+define("_MD_IMGLOSSARY_INTENTRY", "Have a look at this entry at %s");
+define("_MD_IMGLOSSARY_INTENTRYFOUND", "Здесь интересная запись, которую я нашел на %s");
+define("_MD_IMGLOSSARY_INTROREQUEST", "Вы не нашли определение, которое искали? Отправьте нам термин, определение котрого Вы хотите знать, через эту форму. We will look for its definition and add it to the database.");
+define("_MD_IMGLOSSARY_LOOKON", "Поиск");
+define("_MD_IMGLOSSARY_MAINHEAD", "Добро пожаловать на %s");
+define("_MD_IMGLOSSARY_MAININTRO", "In this area of the site you will find our catalogue of glossary entries, as well as the latest entry from each of our categories. Click on a category's name to see all the entries associated with that category, or on an entry's name to read directly the entry. Depending on privileges, you can rate each entry, select notification options or leave your comments.");
+define("_MD_IMGLOSSARY_MAINNOSELECTCAT", "Вы не выбрали категорию");
+define("_MD_IMGLOSSARY_MAINNOTOPICS", "В этой категории нет записей");
+define("_MD_IMGLOSSARY_MESSAGESENT", "Сообщение к %s отправлено.");
+define("_MD_IMGLOSSARY_NO", "Нет");
+define("_MD_IMGLOSSARY_NOENTRIESINCAT", "В этом глоссарии еще нет записей.");
+define("_MD_IMGLOSSARY_NORESULTS", "There were no matches for the search term given. Please use another word and try again.");
+define("_MD_IMGLOSSARY_NOSEARCHTERM", "Вы не указали искомый термин. Пожалуйста, заполните форму для поиска, расположенную на этой странице.");
+define("_MD_IMGLOSSARY_NOTERMSINLETTER", "Нет записей, начинающихся с этого символа.");
+define("_MD_IMGLOSSARY_NOTIFY", "Известить о публикации?");
+define("_MD_IMGLOSSARY_NOTIFYONPUB", "The user has asked to be notified when we do something about the definition.<br />");
+define("_MD_IMGLOSSARY_OTHER", "Другая");
+define("_MD_IMGLOSSARY_POPULARENT", "Популярные записи");
+define("_MD_IMGLOSSARY_POSTED", "Опубликовано");
+define("_MD_IMGLOSSARY_PRINTERFRIENDLY", "Показать эту запись в формате для печати");
+define("_MD_IMGLOSSARY_PRINTTERM", "Формат для печати");
+define("_MD_IMGLOSSARY_RANDOMTERM", "Случайный термин");
+define("_MD_IMGLOSSARY_RECEIVED", "The entry you sent has been received. We'll look at it as soon as possible.");
+define("_MD_IMGLOSSARY_RECEIVEDANDAPPROVED", "The entry you sent has been received. Since the auto-approve option is on, we'll publish it at once.");
+define("_MD_IMGLOSSARY_RECENTENT", "Recent entries");
+define("_MD_IMGLOSSARY_REFERENCE", "Reference<span style='font-size: xx-small; font-weight: normal; display: block;'>(Optionally, you may give the source of your definition here.)</span>");
+define("_MD_IMGLOSSARY_REQRCVD", "Your request has been received. We'll look at it as soon as possible.");
+define("_MD_IMGLOSSARY_REQSEND", "Запрос отправлен");
+define("_MD_IMGLOSSARY_REQTERM", "Термин для определения");
+define("_MD_IMGLOSSARY_REQUESTDEF", "Запрос определения");
+define("_MD_IMGLOSSARY_REQUESTFORM", "Запрос определения");
+define("_MD_IMGLOSSARY_REQUESTSENT", "Ваш запрос переправлен вэбмастеру %s.");
+define("_MD_IMGLOSSARY_RETURN", "Вернуться");
+define("_MD_IMGLOSSARY_RETURN2INDEX", "Перейти в индекс модуля");
+define("_MD_IMGLOSSARY_SEARCH", "Поиск!");
+define("_MD_IMGLOSSARY_SEARCHENTRY", "Поиск");
+define("_MD_IMGLOSSARY_SEARCHHEAD", "Результаты поиска");
+define("_MD_IMGLOSSARY_SECTIONNAME", "Глоссарий");
+define("_MD_IMGLOSSARY_SEEMORE", "Более...");
+define("_MD_IMGLOSSARY_SENDSTORY", "Отправить эту запись другу");
+define("_MD_IMGLOSSARY_SENDTOFRIEND", "Отправить этот термин другу");
+define("_MD_IMGLOSSARY_SENT", "Размещено: ");
+define("_MD_IMGLOSSARY_SENTCONFIRMMAIL", "Your comments have been sent to <b>%s</b> as a confirmation email.");
+define("_MD_IMGLOSSARY_STILLNOTHINGHERE", "<h4 style='margin: 0;'>Glossaries empty</h4>Извините: Мы еще не имеем записей в нашем глоссарии. Вы можете помочь нам by requesting some entries whose definition you'd like to know. If you're a registered user, you can also help by submitting some terms whose definition you know.");
+define("_MD_IMGLOSSARY_SUB_SMNAME", "Заполните эту форму для размещения Вашей записи");
+define("_MD_IMGLOSSARY_SUB_SNEWNAME", "Размещение записи в %s");
+define("_MD_IMGLOSSARY_SUB_SNEWNAMEDESC", "пожалуйста, заполните следующую форму для размещения записи на нашем сайте. As you're well aware, it'll have to be validated by an administrator before it gets published. However, if the administrator has activated the auto-approve setting, your entry will be published instantly.");
+define("_MD_IMGLOSSARY_SUBMIT", "Размещение запроса");
+define("_MD_IMGLOSSARY_SUBMITART", "Размещение записи");
+define("_MD_IMGLOSSARY_SUBMITENTRY", "Разместить запись");
+define("_MD_IMGLOSSARY_SUBMITTED", "Размещено %s. | ");
+define("_MD_IMGLOSSARY_SUBMITTER", "Разместил: ");
+define("_MD_IMGLOSSARY_TERM", "Что искать");
+define("_MD_IMGLOSSARY_TERMS", "Термины");
+define("_MD_IMGLOSSARY_TERMSDEFS", "Термины и определения");
+define("_MD_IMGLOSSARY_THANKS1", "Спасибо Вам за участие!<br />");
+define("_MD_IMGLOSSARY_THANKS2", "Спасибо Вам за запрос определения!");
+define("_MD_IMGLOSSARY_THANKYOU", "Thank you for taking the time to request a definition that will enrich the glossaries of %s.");
+define("_MD_IMGLOSSARY_THEREWERE", "There were %s entries matching the search term: <b>%s</b>.");
+define("_MD_IMGLOSSARY_TIMESREAD", "Прочтено");
+define("_MD_IMGLOSSARY_UNKNOWNERROR", "ОШИБКА.  Returning you to where you were!");
+define("_MD_IMGLOSSARY_URL", "URL<span style='font-size: xx-small; font-weight: normal; display: block;'>(Пожалуйста, введите правильный URL с или без использования HTTP префикса.)</span>");
+define("_MD_IMGLOSSARY_USERMAIL", "E-mail");
+define("_MD_IMGLOSSARY_USERNAME", "Имя");
+define("_MD_IMGLOSSARY_WEBMASTER", "Вэбмастер");
+define("_MD_IMGLOSSARY_WEHAVE", "В настоящее время мы имеем");
+define("_MD_IMGLOSSARY_WHOASKED", "%s asked us to find the definition for: ");
+define("_MD_IMGLOSSARY_WHOSUBMITTED", "%s has submitted the definition for this term: ");
+define("_MD_IMGLOSSARY_YES", "Да");
+
+define("_MD_IMGLOSSARY_OPTIONS", "Опции");
+define("_MD_IMGLOSSARY_DOHTML", "Разрешить HTML?");
+define("_MD_IMGLOSSARY_DOSMILEY", "Разрешить смайлики?");
+define("_MD_IMGLOSSARY_DOXCODE", "Разрешить iCMS коды?");
+define("_MD_IMGLOSSARY_BREAKS", "Разрешить перевод строк?");
+define("_MD_IMGLOSSARY_SUBANDREQ", "Размещено и запрошено терминов");
+define("_MD_IMGLOSSARY_SUB", "Размещено терминов: ");
+define("_MD_IMGLOSSARY_REQ", "Запрошено терминов: ");
+define("_MD_IMGLOSSARY_NOSUB", "Currently there are no submitted terms.");
+define("_MD_IMGLOSSARY_NOREQ", "Currently there are no requested terms.");
+define("_MD_IMGLOSSARY_WRITEHERE", "Напишите здесь определение, пожалуйста.");
+define("_MD_IMGLOSSARY_GUEST","Гость");
+define("_MD_IMGLOSSARY_NOCATSINSYSTEM","В системе нет категорий");
+define("_MD_IMGLOSSARY_REQUESTSUGGEST","(Click the next words to suggest a definition for the term)");
+
+// imGlossary v1.00
+define("_MD_IMGLOSSARY_ADMININDEX", "Администрирование");
+define("_MD_IMGLOSSARY_SUBMITDATE", "Размещено %s. | ");
+define("_MD_IMGLOSSARY_ADDTO", "Добавить к ");
+define("_MD_IMGLOSSARY_PDFTERM", "Создать PDF");
+
+// imGlossary v1.01
+define("_MD_IMGLOSSARY_FEED", "RSS канал");
+?>
