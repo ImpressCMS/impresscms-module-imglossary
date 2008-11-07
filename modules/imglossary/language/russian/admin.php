e1a50e6fd866cbd5302804a898fe32747e2ef80c
algalochkin <contact@impresscms.org>
2008-11-07 19:11:26 +0000

    translate _AM_IMGLOSSARY_ENTRYURLDSC & _AM_IMGLOSSARY_ENTRYREFERENCEDSC
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imglossary/trunk@6542 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- modules/imglossary/language/russian/admin.php
+++ modules/imglossary/language/russian/admin.php
@@ -30,7 +30,7 @@ define("_AM_IMGLOSSARY_ANON", "Гость");
 define("_AM_IMGLOSSARY_AUTHENTRY", "Разрешить подчинение");
 define("_AM_IMGLOSSARY_AUTHOR", "Автор");
 define("_AM_IMGLOSSARY_AUTHORIZE", "Разрешить");
-define("_AM_IMGLOSSARY_BACK2IDX", "Отменено. Возвращамся обратно к списку");
+define("_AM_IMGLOSSARY_BACK2IDX", "Отменено. Возвращаемся обратно к списку");
 define("_AM_IMGLOSSARY_BLOCK", " Добавить в блок записей?");
 define("_AM_IMGLOSSARY_BLOCKS", "Блоки");
 define("_AM_IMGLOSSARY_BREAKS", " Использовать конверсию перевода строки?");
@@ -74,19 +74,19 @@ define("_AM_IMGLOSSARY_ENTRYISDELETED", "Запись %s удалена.");
 define("_AM_IMGLOSSARY_ENTRYISOFF", "Запись отключена");
 define("_AM_IMGLOSSARY_ENTRYISON", "Запись подключена");
 define("_AM_IMGLOSSARY_ENTRYMODIFIED", "Запись отредактирована!");
-define("_AM_IMGLOSSARY_ENTRYNOTCREATED", "Извините. It was not possible to create this entry!");
-define("_AM_IMGLOSSARY_ENTRYNOTUPDATED", "Извините. It was not possible to update the entry!");
+define("_AM_IMGLOSSARY_ENTRYNOTCREATED", "Извините. Невозможно создать эту запись!");
+define("_AM_IMGLOSSARY_ENTRYNOTUPDATED", "Извините. Невозможно обновить эту запись!");
 define("_AM_IMGLOSSARY_ENTRYREFERENCE", "Reference");
 define("_AM_IMGLOSSARY_ENTRYTERM", "Термин");
 define("_AM_IMGLOSSARY_ENTRYURL", "Связанный сайт");
-define("_AM_IMGLOSSARY_FILEEXISTS", "A file with that name already exists on server. Please choose another one!");
+define("_AM_IMGLOSSARY_FILEEXISTS", "Файл с таким именем уже имеетсы на сервере. Пожалуйста, выберите другое имя файла!");
 define("_AM_IMGLOSSARY_GOMOD", "Перейти в модуль");
 define("_AM_IMGLOSSARY_HELP", "Помощь");
 define("_AM_IMGLOSSARY_ID", "Id");
 define("_AM_IMGLOSSARY_INDEX", "Индекс");
 define("_AM_IMGLOSSARY_INVENTORY", "Резюме");
 define("_AM_IMGLOSSARY_MODCAT", "Редактирование существующей категории");
-define("_AM_IMGLOSSARY_MODADMIN", " Module Admin: ");
+define("_AM_IMGLOSSARY_MODADMIN", " Администратор модуля: ");
 define("_AM_IMGLOSSARY_MODENTRY", "Редактирование записи");
 define("_AM_IMGLOSSARY_MODIFY", "Редактирование");
 define("_AM_IMGLOSSARY_MODIFYCAT", "Редактирование категории");
@@ -110,7 +110,7 @@ define("_AM_IMGLOSSARY_SHOWCATS", "Категории");
 define("_AM_IMGLOSSARY_SHOWENTRIES", "Записи");
 define("_AM_IMGLOSSARY_SHOWREQUESTS", "Запросы");
 define("_AM_IMGLOSSARY_SHOWSUBMISSIONS", "Размещения");
-define("_AM_IMGLOSSARY_SINGLECAT", "The module is defined to have a single category. You have no reason to be here.");
+define("_AM_IMGLOSSARY_SINGLECAT", "Модуль имеет единственную категорию. У Вас нет причин находиться здесь.");
 define("_AM_IMGLOSSARY_STATUS", "Статус");
 define("_AM_IMGLOSSARY_SUBMITS", "Размещения");
 define("_AM_IMGLOSSARY_SUBMITTER", "Кто разместил");
@@ -125,23 +125,23 @@ define("_AM_IMGLOSSARY_WRITEHERE", "Напишите здесь определе
 define("_AM_IMGLOSSARY_YES", "Да");
 //mondarse
 define("_AM_IMGLOSSARY_IMPORT", "Импорт");
-define("_AM_IMGLOSSARY_IMPORTWARN", "Предупреждение!! Прежде чем продолжить, сохраните Вашу базу данных.<br />Версия скрипта импорта имеет статус Альфа, и это может привести к порче данных.");
+define("_AM_IMGLOSSARY_IMPORTWARN", "Предупреждение! Прежде чем продолжить, сохраните Вашу базу данных.<br />Версия скрипта импорта имеет статус Альфа, и это может привести к порче данных.");
 
 // Taken from importdictionary091.php (McDonald)
 define("_AM_IMGLOSSARY_IMPDICT_01","База данных для импорта отсутствует или пуста!");
-define("_AM_IMGLOSSARY_IMPDICT_02","imGlossary Entries Import Script");
+define("_AM_IMGLOSSARY_IMPDICT_02","Скрипт импорта записей в imGlossary");
 define("_AM_IMGLOSSARY_IMPDICT_03","Импортировать из Dictionary вер. 0.92");
 define("_AM_IMGLOSSARY_IMPDICT_04","Ошибка: #");
-define("_AM_IMGLOSSARY_IMPDICT_05","Dictionary ID: ");
-define("_AM_IMGLOSSARY_IMPDICT_06","imGlossary ID: ");
-define("_AM_IMGLOSSARY_IMPDICT_07","Error while moving Comments from Dictionary to imGlossary module.");
-define("_AM_IMGLOSSARY_IMPDICT_08","Comments successfully moved from Dictionary to imGlossary.");
-define("_AM_IMGLOSSARY_IMPDICT_09","Incorrectly: ");
-define("_AM_IMGLOSSARY_IMPDICT_10","Processed: ");
+define("_AM_IMGLOSSARY_IMPDICT_05","ID модуля Dictionary: ");
+define("_AM_IMGLOSSARY_IMPDICT_06","ID модуля imGlossary: ");
+define("_AM_IMGLOSSARY_IMPDICT_07","Ошибка во время перемещения комментариев из модуля Dictionary в imGlossary.");
+define("_AM_IMGLOSSARY_IMPDICT_08","Комментарии перемещены из Dictionary в imGlossary.");
+define("_AM_IMGLOSSARY_IMPDICT_09","Некорректно: ");
+define("_AM_IMGLOSSARY_IMPDICT_10","В процессе: ");
 define("_AM_IMGLOSSARY_IMPDICT_11","Вернуться к администрированию");
 define("_AM_IMGLOSSARY_IMPDICT_12","Модуль Dictionary не найден на этом сайте.");
 
-define("_AM_IMGLOSSARY_NOCOLEXISTS", "Извините, there are no categories defined as of yet.<br />Please contact the site administrator and tell him about this."); // This was originally in the main.php file
+define("_AM_IMGLOSSARY_NOCOLEXISTS", "Извините, категории здесь пока не определены.<br />Пожалуйста, свяжитесь с администратором сайта и сообщите ему об этом."); // This was originally in the main.php file
 
 // imGlossary v1.00
 define("_AM_IMGLOSSARY_SHOWOFFLINE", "Автономный режим");
@@ -150,18 +150,18 @@ define("_AM_IMGLOSSARY_ABOUT", "О модуле");
 define("_AM_IMGLOSSARY_UPDATEMOD", "Обновить модуль");
 
 // Import 
-define("_AM_IMGLOSSARY_MODULEHEADIMPORTWB", "Wordbook &mdash;&gt; imGlossary import script");
-define("_AM_IMGLOSSARY_MODULEHEADIMPORT", "Dictionary 0.91 &mdash;&gt; imGlossary import script");
-define("_AM_IMGLOSSARY_MODULEHEADIMPORTGLO", "Glossaire &mdash;&gt; imGlossary import script");
-define("_AM_IMGLOSSARY_MODULEHEADIMPORTXWO", "XWords &mdash;&gt; imGlossary import script");
-define("_AM_IMGLOSSARY_MODULEHEADIMPORTWW", "Wiwimod &mdash;&gt; imGlossary import script");
+define("_AM_IMGLOSSARY_MODULEHEADIMPORTWB", "Скрипт импорта Wordbook &mdash;&gt; imGlossary");
+define("_AM_IMGLOSSARY_MODULEHEADIMPORT", "Скрипт импорта Dictionary 0.91 &mdash;&gt; imGlossary");
+define("_AM_IMGLOSSARY_MODULEHEADIMPORTGLO", "Скрипт импорта Glossaire &mdash;&gt; imGlossary");
+define("_AM_IMGLOSSARY_MODULEHEADIMPORTXWO", "Скрипт импорта XWords &mdash;&gt; imGlossary");
+define("_AM_IMGLOSSARY_MODULEHEADIMPORTWW", "Скрипт импорта Wiwimod &mdash;&gt; imGlossary");
 define("_AM_IMGLOSSARY_MODULEIMPORTEMPTY10", "База данных отсутствует или пуста!");
 define("_AM_IMGLOSSARY_MODULEIMPORTERCOM", "Комментарии перемещены в imGlossary");
-define("_AM_IMGLOSSARY_MODULEIMPORTERNOCOM", "Ошибкаво время перемещения Комментариев в imGlossary");
+define("_AM_IMGLOSSARY_MODULEIMPORTERNOCOM", "Ошибка во время перемещения комментариев в imGlossary");
 define("_AM_IMGLOSSARY_IMPORTDELWB","Удалить записи перед импортом? ");
-define("_AM_IMGLOSSARY_NOOTHERMODS", "No corresponding modules could be located!");
+define("_AM_IMGLOSSARY_NOOTHERMODS", "Нет соответствующих модулей!");
 
 // imGlossary v1.01 RC-1
-define("_AM_IMGLOSSARY_ENTRYURLDSC", "Please enter a valid URL with or without the HTTP prefix.");
-define("_AM_IMGLOSSARY_ENTRYREFERENCEDSC", "Write here the source of your definition, such as a book, article, or person.");
+define("_AM_IMGLOSSARY_ENTRYURLDSC", "Пожалуйста, введите корректный URL с указанием или без указания HTTP префикса.");
+define("_AM_IMGLOSSARY_ENTRYREFERENCEDSC", "Напишите здесь исходный текст Вашего определения, такого как книга, статья или персона.");
 ?>
