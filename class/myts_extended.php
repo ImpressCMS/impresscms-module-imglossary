a89b44a98feb7d288c08db3f3a71e5d8ead705af
Ronald <pietjebell31@hotmail.com>
2008-11-16 16:07:16 +0000

    Adding class
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imglossary/trunk/modules/imglossary@6982 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ class/myts_extended.php
@@ -0,0 +1,38 @@
+<?php
+/**
+* imGlossary - a multicategory glossary for ImpressCMS
+*
+* Based upon Wordbook 1.16
+*
+* File: /class/myts_extended.php
+*
+* @copyright		http://www.xoops.org/ The XOOPS Project
+* @copyright		XOOPS_copyrights.txt
+* @copyright		http://www.impresscms.org/ The ImpressCMS Project
+* @license		GNU General Public License (GPL)
+*				a copy of the GNU license is enclosed.
+* ----------------------------------------------------------------------------------------------------------
+* @package		WF-Links 
+* @since			1.03
+* @author		John N
+* ----------------------------------------------------------------------------------------------------------
+* 				WF-Links 
+* @since			1.03b and 1.03c
+* @author		McDonald
+* ----------------------------------------------------------------------------------------------------------
+* 				imGlossary
+* @since			1.01
+* @author		McDonald
+* @version		$Id$
+*/
+
+class imglossaryTextSanitizer extends MyTextSanitizer {
+    function imglossaryTextSanitizer() {
+    } 
+
+	function htmlSpecialCharsStrip( $text ) {
+		return $this -> htmlSpecialChars( $this -> stripSlashesGPC( $text) );
+    } 
+} 
+
+?>
