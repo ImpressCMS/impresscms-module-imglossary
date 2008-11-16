a90da8c753b63b92db8faf7d6492f0d9bf53c0f3
GibaPhp <gibaphp.xoopstotal@gmail.com>
2008-11-16 12:02:52 +0000

    Portuguesebr - imGlossary - Updated with last version and Finished Translation...
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imglossary/trunk/modules/imglossary@6970 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- language/portuguesebr/blocks.php
+++ language/portuguesebr/blocks.php
@@ -1,12 +1,27 @@
 <?php
 /**
- * $Id: blocks.php v 1.0 8 May 2004 hsalazar Exp $
- * Module: Wordbook - a multicategory glossary
- * Version: v 1.00
- * Release Date: 8 May 2004
- * Author: hsalazar
- * Licence: GNU
- */
+* imGlossary - a multicategory glossary for ImpressCMS
+*
+* Based upon Wordbook 1.16
+*
+* File: language/portuguesebr/blocks.php
+*
+* @copyright		http://www.xoops.org/ The XOOPS Project
+* @copyright		XOOPS_copyrights.txt
+* @copyright		http://www.impresscms.org/ The ImpressCMS Project
+* @license		GNU General Public License (GPL)
+*				a copy of the GNU license is enclosed.
+* ----------------------------------------------------------------------------------------------------------
+* @package		Wordbook - a multicategory glossary
+* @since			1.16
+* @author		hsalazar
+* ----------------------------------------------------------------------------------------------------------
+* 				imGlossary - a multicategory glossary
+* @since			1.00
+* @author		modified by McDonald
+* @version		$Id$
+*/
+
 // Blocks
 define("_MB_IMGLOSSARY_DATE", "Data de Publicação");
 define("_MB_IMGLOSSARY_DELTERM", "Apagar este Termo");
