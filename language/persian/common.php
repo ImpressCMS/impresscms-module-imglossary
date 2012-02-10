0140ac2df94fd5ceb326baef2e482d5147895964
Ronald <pietjebell31@hotmail.com>
2012-02-10 10:33:27 +0000

    Removed redundant entries in language file
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imglossary/trunk/modules/imglossary@23466 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- language/persian/common.php
+++ language/persian/common.php
@@ -22,31 +22,16 @@
 * @version		$Id$
 */
 
-defined("ICMS_ROOT_PATH") or die("ICMS root path not defined");
+defined( 'ICMS_ROOT_PATH' ) or die( 'ICMS root path not defined' );
 
-define("_CO_IMGLOSSARY_ENTRIES_TERM", "Term");
-define("_CO_IMGLOSSARY_ENTRIES_DESCRIPTION_DSC", "Summary description of this feed.");
-define("_CO_IMGLOSSARY_ENTRIES_TITLE", "Title");
-define("_CO_IMGLOSSARY_ENTRIES_TITLE_DSC", "Name of this feed.");
-define("_CO_IMGLOSSARY_ENTRIES_ENTRYID", "ID");
-define("_CO_IMGLOSSARY_ENTRIES_IDENTIFIER_DSC", "Enter the target URL of the RSS feed to which you wish to subscribe.");
-define("_CO_IMGLOSSARY_ENTRIES_DATESUB", "Created");
-define("_CO_IMGLOSSARY_ENTRIES_ITEM_LIMIT_DSC", "Maximum number of items that will be retrieve for this feed.");
-define("_CO_IMGLOSSARY_ENTRIES_ONLINE_STATUS", "Online?");
-define("_CO_IMGLOSSARY_ENTRIES_LAST_UPDATE", "Last update: ");
-define("_CO_IMGLOSSARY_ENTRIES_LAST_UPDATE_DSC", "The last time this feed was refreshed.");
-define("_CO_IMGLOSSARY_ENTRIES_WEIGHT", "Weight");
-define("_CO_IMGLOSSARY_ENTRIES_WEIGHT_DSC", "Position (sort order) in the feed list.");
-define("_CO_IMGLOSSARY_ENTRIES_ONLINE_STATUS_DSC", "Toggle this feed online or offline.");
-define("_CO_IMGLOSSARY_ENTRIES_ONLINE", "Feed online.");
-define("_CO_IMGLOSSARY_ENTRIES_OFFLINE", "Status");
-define("_CO_IMGLOSSARY_ENTRIES_SWITCH_OFFLINE", "Toggle this feed offline");
-define("_CO_IMGLOSSARY_ENTRIES_SWITCH_ONLINE", "Toggle this feed online");
-define("_CO_IMGLOSSARY_ENTRIES_UID", "Submitter");
-define("_CO_IMGLOSSARY_CATS_CATEGORYID", "ID");
-define("_CO_IMGLOSSARY_CATS_NAME", "Category name");
-define("_CO_IMGLOSSARY_CATS_DESCRIPTION", "Category description");
-define("_CO_IMGLOSSARY_CATS_WEIGHT", "Weight");
-define("_CO_READER_LATEST_POSTS", "Latest posts");
-define("_CO_READER_NEWSFEEDS", "Newsfeeds");
-define("_CO_READER_SUBSCRIBE_TO", "Subscribe to ");
+define( '_CO_IMGLOSSARY_ENTRIES_TERM', 'Term' );
+define( '_CO_IMGLOSSARY_ENTRIES_DESCRIPTION_DSC', 'Summary description of this feed.' );
+define( '_CO_IMGLOSSARY_ENTRIES_TITLE', 'Title' );
+define( '_CO_IMGLOSSARY_ENTRIES_ENTRYID', 'ID' );
+define( '_CO_IMGLOSSARY_ENTRIES_DATESUB', 'Created' );
+define( '_CO_IMGLOSSARY_ENTRIES_OFFLINE', 'Status' );
+define( '_CO_IMGLOSSARY_ENTRIES_UID', 'Submitter' );
+define( '_CO_IMGLOSSARY_CATS_CATEGORYID', 'ID' );
+define( '_CO_IMGLOSSARY_CATS_NAME', 'Category name' );
+define( '_CO_IMGLOSSARY_CATS_DESCRIPTION', 'Category description' );
+define( '_CO_IMGLOSSARY_CATS_WEIGHT', 'Weight' );
