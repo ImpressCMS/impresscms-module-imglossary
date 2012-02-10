b8e59270c39affa350013352bbe74811bf526b70
Ronald <pietjebell31@hotmail.com>
2012-02-10 22:55:31 +0000

    Updated block code
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imglossary/trunk@23484 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- modules/imglossary/blocks/entries_new.php
+++ modules/imglossary/blocks/entries_new.php
@@ -6,18 +6,18 @@
 *
 * File: blocks/entries_new.php
 *
-* @copyright		http://www.xoops.org/ The XOOPS Project
-* @copyright		XOOPS_copyrights.txt
-* @copyright		http://www.impresscms.org/ The ImpressCMS Project
+* @copyright	http://www.xoops.org/ The XOOPS Project
+* @copyright	XOOPS_copyrights.txt
+* @copyright	http://www.impresscms.org/ The ImpressCMS Project
 * @license		GNU General Public License (GPL)
 *				a copy of the GNU license is enclosed.
 * ----------------------------------------------------------------------------------------------------------
 * @package		Wordbook - a multicategory glossary
-* @since			1.16
+* @since		1.16
 * @author		hsalazar
 * ----------------------------------------------------------------------------------------------------------
 * 				imGlossary - a multicategory glossary
-* @since			1.00
+* @since		1.00
 * @author		modified by McDonald
 * @version		$Id$
 */
@@ -46,9 +46,8 @@ function b_entries_new_show( $options )	{
 		// If there are definitions
 		while ( list( $entryID, $term, $datesub ) = icms::$xoopsDB -> fetchRow( $result ) ) {
 			$newentries = array();
-			$xoopsModule = XoopsModule::getByDirname( $glossdirname );
-			$linktext = $myts -> makeTboxData4Show( $term );
-			$newentries['dir'] = $xoopsModule -> dirname();
+			$linktext = icms_core_DataFilter::htmlSpecialchars( $term );
+			$newentries['dir'] = $module_name;
 			$newentries['linktext'] = $linktext;
 			$newentries['id'] = $entryID;
 			$newentries['date'] = "<span style='font-size: x-small;'>" . formatTimestamp( $datesub, $options[2] ) . "</span>";
@@ -59,32 +58,32 @@ function b_entries_new_show( $options )	{
 	return $block;
 } 
 
-function b_entries_new_edit( $options )	{
+function b_entries_new_edit( $options ) {
 	$form = "" . _MB_IMGLOSSARY_ORDER . "&nbsp;<select name='options[]'>";
 
 	$form .= "<option value='datesub'";
-	if ( $options[0] == "datesub" )	{
+	if ( $options[0] == "datesub" ) {
 		$form .= " selected='selected'";
-	} 
-	
+	}
+
 	$form .= ">" . _MB_IMGLOSSARY_DATE . "</option>\n";
 	$form .= "<option value='counter'";
-	
-	if ( $options[0] == "counter" )	{
+
+	if ( $options[0] == "counter" ) {
 		$form .= " selected='selected'";
-	} 
-	
+	}
+
 	$form .= ">" . _MB_IMGLOSSARY_HITS . "</option>\n";
 	$form .= "<option value='weight'";
 	if ( $options[0] == "weight" ) {
 		$form .= " selected='selected'";
-	} 
-	
+	}
+
 	$form .= ">" . _MB_IMGLOSSARY_WEIGHT . "</option>\n";
 	$form .= "</select>\n";
 	$form .= "&nbsp;<br />" . _MB_IMGLOSSARY_DISP . "&nbsp;<input type='text' name='options[]' value='" . $options[1] . "' />&nbsp;" . _MB_IMGLOSSARY_TERMS . "";
 	$form .= "&nbsp;<br />" . _MB_IMGLOSSARY_DATEFORMAT . "&nbsp;<input type='text' name='options[]' value='" . $options[2] . "' />&nbsp;" . _MB_IMGLOSSARY_DATEFORMATMANUAL;
 
 	return $form;
-} 
+}
 ?>
