0e1dab8f269e267abbf2151d76001be975450c46
Ronald <pietjebell31@hotmail.com>
2012-02-11 11:49:42 +0000

    Cleaned up code
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imglossary/trunk@23491 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- modules/imglossary/include/imglossary_moduleabout.php
+++ modules/imglossary/include/imglossary_moduleabout.php
@@ -52,30 +52,28 @@ class imGlossaryModuleAbout {
 
 		icms_loadLanguageFile( icms::$module -> getVar( 'dirname' ), 'modinfo' );
 		icms_loadLanguageFile( icms::$module -> getVar( 'dirname' ), 'moduleabout' );
-		
-		$this -> _aboutTitle = $aboutTitle;
 
-		$this -> _lang_developer_contributor = _MODABOUT_IMGLOSSARY_DEVELOPER_CONTRIBUTOR;
-		$this -> _lang_developer_website = _MODABOUT_IMGLOSSARY_DEVELOPER_WEBSITE;
-		$this -> _lang_developer_email = _MODABOUT_IMGLOSSARY_DEVELOPER_EMAIL;
-		$this -> _lang_developer_credits = _MODABOUT_IMGLOSSARY_DEVELOPER_CREDITS;
-		$this -> _lang_module_info = _MODABOUT_IMGLOSSARY_MODULE_INFO;
-		$this -> _lang_module_status = _MODABOUT_IMGLOSSARY_MODULE_STATUS;
-		$this -> _lang_module_release_date =_MODABOUT_IMGLOSSARY_MODULE_RELEASE_DATE ;
-		$this -> _lang_module_demo = _MODABOUT_IMGLOSSARY_MODULE_DEMO;
-		$this -> _lang_module_support = _CO_ICMS_MODULE_SUPPORT;
-		$this -> _lang_module_bug = _MODABOUT_IMGLOSSARY_MODULE_BUG;
-		$this -> _lang_module_submit_bug = _MODABOUT_IMGLOSSARY_MODULE_SUBMIT_BUG;
-		$this -> _lang_module_feature = _MODABOUT_IMGLOSSARY_MODULE_FEATURE;
-		$this -> _lang_module_submit_feature = _CO_ICMS_MODULE_SUBMIT_FEATURE;
-		$this -> _lang_module_disclaimer = _MODABOUT_IMGLOSSARY_MODULE_DISCLAIMER;
-		$this -> _lang_author_word = _MODABOUT_IMGLOSSARY_AUTHOR_WORD;
-		$this -> _lang_version_history = _MODABOUT_IMGLOSSARY_VERSION_HISTORY;
+		$this -> _aboutTitle					= $aboutTitle;
+		$this -> _lang_developer_contributor	= _MODABOUT_IMGLOSSARY_DEVELOPER_CONTRIBUTOR;
+		$this -> _lang_developer_website		= _MODABOUT_IMGLOSSARY_DEVELOPER_WEBSITE;
+		$this -> _lang_developer_email			= _MODABOUT_IMGLOSSARY_DEVELOPER_EMAIL;
+		$this -> _lang_developer_credits		= _MODABOUT_IMGLOSSARY_DEVELOPER_CREDITS;
+		$this -> _lang_module_info				= _MODABOUT_IMGLOSSARY_MODULE_INFO;
+		$this -> _lang_module_status			= _MODABOUT_IMGLOSSARY_MODULE_STATUS;
+		$this -> _lang_module_release_date		= _MODABOUT_IMGLOSSARY_MODULE_RELEASE_DATE ;
+		$this -> _lang_module_demo				= _MODABOUT_IMGLOSSARY_MODULE_DEMO;
+		$this -> _lang_module_support			= _CO_ICMS_MODULE_SUPPORT;
+		$this -> _lang_module_bug				= _MODABOUT_IMGLOSSARY_MODULE_BUG;
+		$this -> _lang_module_submit_bug		= _MODABOUT_IMGLOSSARY_MODULE_SUBMIT_BUG;
+		$this -> _lang_module_feature			= _MODABOUT_IMGLOSSARY_MODULE_FEATURE;
+		$this -> _lang_module_submit_feature	= _CO_ICMS_MODULE_SUBMIT_FEATURE;
+		$this -> _lang_module_disclaimer		= _MODABOUT_IMGLOSSARY_MODULE_DISCLAIMER;
+		$this -> _lang_author_word				= _MODABOUT_IMGLOSSARY_AUTHOR_WORD;
+		$this -> _lang_version_history			= _MODABOUT_IMGLOSSARY_VERSION_HISTORY;
 	}
 
 	/**
 	 * Santizie a value 
-	 *
 	 * @param string $value to be sanitized
 	 * @return string santizied value
 	 */
@@ -85,12 +83,8 @@ class imGlossaryModuleAbout {
 
 	/**
 	 * Render the whole About page of a module
-	 *
 	 */
 	function render() {
-		/**
-		 * @todo make the output XHTML compliant
-		 */
 
 		global $icmsConfig;
 
@@ -104,23 +98,20 @@ class imGlossaryModuleAbout {
 		include_once ICMS_ROOT_PATH . '/class/template.php';
 
 		$this -> _tpl = new icms_view_Tpl();
-
 		$this -> _tpl -> assign( 'module_url', ICMS_URL . '/modules/' . icms::$module -> getVar( 'dirname' ) . '/' );
 		$this -> _tpl -> assign( 'module_image', $versioninfo -> getInfo( 'image' ) );
 		$this -> _tpl -> assign( 'module_name', $versioninfo -> getInfo( 'name' ) );
 		$this -> _tpl -> assign( 'module_version', $versioninfo -> getInfo( 'version' ) );
 		$this -> _tpl -> assign( 'module_status_version', $versioninfo -> getInfo( 'status_version' ) );
 
-		// Left headings...
+		// Left headings
 		if ( $versioninfo -> getInfo( 'author_realname' ) != '' ) {
 			$author_name = $versioninfo -> getInfo( 'author' ) . ' (' . $versioninfo -> getInfo( 'author_realname' ) . ')';
 		} else {
 			$author_name = $versioninfo -> getInfo( 'author' );
 		}
 		$this -> _tpl -> assign( 'module_author_name', $author_name );
-
 		$this -> _tpl -> assign( 'module_license', $versioninfo -> getInfo( 'license' ) );
-
 		$this -> _tpl -> assign( 'module_credits', $versioninfo -> getInfo( 'credits' ) );
 
 		// Developers Information
@@ -132,14 +123,12 @@ class imGlossaryModuleAbout {
 
 		$people = $versioninfo -> getInfo( 'people' );
 		if ( $people ) {
-
 			$this -> _tpl -> assign( 'module_people_developers', isset( $people['developers'] ) ? array_map( array( $this, 'sanitize' ), $people['developers'] ) : false );
 			$this -> _tpl -> assign( 'module_people_testers', isset( $people['testers'] ) ? array_map( array( $this, 'sanitize' ), $people['testers'] ) : false );
 			$this -> _tpl -> assign( 'module_people_translators', isset( $people['translators'] ) ? array_map( array( $this, 'sanitize' ), $people['translators'] ) : false );
 			$this -> _tpl -> assign( 'module_people_documenters', isset( $people['documenters'] ) ? array_map( array( $this, 'sanitize' ), $people['documenters'] ) : false );
 			$this -> _tpl -> assign( 'module_people_other', isset( $people['other'] ) ? array_map( array( $this, 'sanitize' ), $people['other'] ) : false );
 		}
-		//$this->_tpl->assign('module_developers', $versioninfo->getInfo('developer_email'));
 
 		// Module Development information
 		$this -> _tpl->assign( 'module_date', $versioninfo -> getInfo( 'date' ) );
@@ -150,7 +139,7 @@ class imGlossaryModuleAbout {
 		$this -> _tpl->assign( 'module_support_site_name', $versioninfo -> getInfo( 'support_site_name' ) );
 		$this -> _tpl->assign( 'module_submit_bug', $versioninfo -> getInfo( 'submit_bug' ) );
 		$this -> _tpl->assign( 'module_submit_feature', $versioninfo -> getInfo( 'submit_feature' ) );
-		
+
 		// Manual
 		$manual =$versioninfo -> getInfo( 'manual' );
 		if ( $manual ) {
@@ -163,29 +152,27 @@ class imGlossaryModuleAbout {
 		// Author's note
 		$this -> _tpl -> assign( 'module_author_word', $versioninfo -> getInfo( 'author_word' ) );
 
-	    // For changelog thanks to 3Dev
-	    //global icms::$module;
-	    $filename = ICMS_ROOT_PATH . '/modules/' . icms::$module -> getVar( 'dirname' ) . '/changelog.txt';
-	    if ( is_file( $filename ) ) {
+		// For changelog thanks to 3Dev
+		$filename = ICMS_ROOT_PATH . '/modules/' . icms::$module -> getVar( 'dirname' ) . '/changelog.txt';
+		if ( is_file( $filename ) ) {
+			$filesize = filesize( $filename );
+			$handle = fopen( $filename, 'r' );
+			$this -> _tpl -> assign( 'module_version_history', icms_core_DataFilter::checkVar( fread( $handle, $filesize ), 'text', 'output' ) );
+			fclose( $handle );
+		}
 
-	        $filesize = filesize( $filename );
-	        $handle = fopen( $filename, 'r' );
-	        $this -> _tpl -> assign( 'module_version_history', icms_core_DataFilter::checkVar( fread( $handle, $filesize ), 'text', 'output' ) );
-	        fclose( $handle );
-	    }
-		
 		// For license thanks to 3Dev
 		if ( file_exists( ICMS_ROOT_PATH . '/modules/' . icms::$module -> getVar( 'dirname' ) . '/license/' . $icmsConfig['language'] . '_license.txt' ) ) {
 			$filename = ICMS_ROOT_PATH . '/modules/' . icms::$module -> getVar( 'dirname' ) . '/license/' . $icmsConfig['language'] . '_license.txt';
 		} elseif ( file_exists( ICMS_ROOT_PATH . '/modules/' . icms::$module -> getVar( 'dirname' ) . '/license.txt' ) ) {
 			$filename = ICMS_ROOT_PATH . '/modules/' . icms::$module -> getVar( 'dirname' ) . '/license.txt';
 		}
-	    if( is_file( $filename ) ) {
-	        $filesize = filesize( $filename );
-	        $handle = fopen( $filename, 'r' );
-	        $this -> _tpl -> assign( 'module_license_txt', icms_core_DataFilter::checkVar( fread( $handle, $filesize ), 'text', 'output' ) );
-	        fclose($handle);
-	    }
+		if( is_file( $filename ) ) {
+			$filesize = filesize( $filename );
+			$handle = fopen( $filename, 'r' );
+			$this -> _tpl -> assign( 'module_license_txt', icms_core_DataFilter::checkVar( fread( $handle, $filesize ), 'text', 'output' ) );
+			fclose($handle);
+		}
 
 		$this -> _tpl -> display( ICMS_ROOT_PATH . '/modules/' . icms::$module -> getVar( 'dirname' ) . '/templates/imglossary_moduleabout.html' );
 
