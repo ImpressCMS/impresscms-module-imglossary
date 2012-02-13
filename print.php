6313f156472f996864b9d7fe9f6645db93dba362
Ronald <pietjebell31@hotmail.com>
2012-02-13 11:12:30 +0000

    Improved layout of print output
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imglossary/trunk/modules/imglossary@23524 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- print.php
+++ print.php
@@ -38,8 +38,6 @@ if ( empty( $entryID ) ) {
 
 function printPage( $entryID ) {
 
-	$glossdirname = basename( dirname( __FILE__ ) );
-
 	global $icmsConfig;
 
 	$result1 = icms::$xoopsDB -> query( 'SELECT * FROM ' . icms::$xoopsDB -> prefix( 'imglossary_entries' ) . ' WHERE entryID=' . $entryID . ' AND submit=0 ORDER BY datesub' );
@@ -73,27 +71,26 @@ function printPage( $entryID ) {
 	echo '<meta name="description" content="' . $icmsConfig['slogan'] . '" />';
 	echo '<meta name="generator" content="' . ICMS_VERSION_NAME . '" />';
 
-	echo '<body bgcolor="#ffffff" text="#000000">
-			<font face="Verdana, Arial, Helvetica, sans-serif">
-			<div style="width: 650px; border: 1px solid #000; padding: 20px;">
-				<div style="text-align: center; display: block; padding-bottom: 12px; margin: 0 0 6px 0; border-bottom: 2px solid #ccc;">
+	echo '<body>
+			<div style="background-color: #fff; color: #000; width: 650px; border: 1px solid #000; padding: 20px; font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 11px;">
+				<div style="text-align: center; display: block; padding-bottom: 12px; margin: 0 0 6px 0; border-bottom: 1px solid #ccc;">
 					<img src="' . ICMS_URL . '/modules/' . icms::$module -> getVar( 'dirname' ) . '/images/imglossary_logo.png" border="0" alt="" />
 					<h2 style="margin: 0;">' . $term . '</h2>
-				</div>
-			</div>';
-		
+				</div>';
+
 	if ( icms::$module -> config['multicats'] == 1 ) {
 		echo '<div>' . _MD_IMGLOSSARY_ENTRYCATEGORY . '<b>' . $categoryname . '</b></div>';
 	}
-	echo '<div style="padding-bottom: 6px; border-bottom: 1px solid #ccc;">' . _MD_IMGLOSSARY_SUBMITTER . '<b>' . $authorname . '</b></div><br />
-			<h3 style="margin: 0;">' . $term . '</h3>
-			<p>' . $definition . '</p>
-			<div style="padding-top: 12px; border-top: 2px solid #ccc;">
-				<b>' . _MD_IMGLOSSARY_SENT . '</b>' . $datetime . '<br />
-			</div>
+
+	echo '		<div style="padding-bottom: 6px; border-bottom: 1px solid #ccc;">
+					' . _MD_IMGLOSSARY_SUBMITTER . '<b>' . $authorname . '</b><br />
+					' . _MD_IMGLOSSARY_SENT . '<b>' . $datetime . '</b>
+				</div>
+				<br />
+				<h3 style="margin: 0;">' . $term . '</h3>
+				<div>' . $definition . '</div>
 			</div>
 			<br />
-			</font>
 		</body>
 		</html>';
 }
