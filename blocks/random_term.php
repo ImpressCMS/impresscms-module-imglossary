2dde6c98bac4f84ce62e7670cb9e686a3748c971
Ronald <pietjebell31@hotmail.com>
2012-02-14 00:12:38 +0000

    Fixed random block
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imglossary/trunk/modules/imglossary@23534 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- blocks/random_term.php
+++ blocks/random_term.php
@@ -25,7 +25,6 @@
 function b_entries_random_show() {
 
 	global $icmsConfig;
-	$myts =& MyTextSanitizer::getInstance();
 
 	include_once ICMS_ROOT_PATH . '/modules/' . basename( dirname( dirname( __FILE__ ) ) ) . '/include/functions.php';
 
@@ -58,13 +57,13 @@ function b_entries_random_show() {
 		if ( !XOOPS_USE_MULTIBYTES ) {
 			$definition = icms_core_DataFilter::icms_substr( $myrow['definition'], 0, $imgConfig['rndlength'] -1, '...' );
 		}
-		$definition = $myts -> displayTarea( $definition, $myrow['html'], $myrow['smiley'], $myrow['xcodes'], 0, $myrow['breaks'] );
+		$definition = icms_core_DataFilter::checkVar( $definition, 'html', 'output' );
 		$categoryID = $myrow['categoryID'];
-		
+
 		$result_cat = icms::$xoopsDB -> query( 'SELECT categoryID, name FROM ' . icms::$xoopsDB -> prefix( 'imglossary_cats' ) . ' WHERE categoryID=' . $categoryID );
 		list( $categoryID, $name ) = icms::$xoopsDB -> fetchRow( $result_cat );
 		$category = '<div style="font-size: x-small; font-weight: normal; padding: 4px; margin: 0;"><a href="' . ICMS_URL . '/modules/' . basename( dirname( dirname( __FILE__ ) ) ) . '/category.php?categoryID=' . $categoryID . '">' . $name . '</a></div>';
-		
+
 		$userlinks = '
 		<a href="' . ICMS_URL . '/modules/' . basename( dirname( dirname( __FILE__ ) ) ) . '/makepdf.php?entryID=' . $entryID . '" target="_blank">
 			<img src="' . ICMS_URL . '/modules/' . basename( dirname( dirname( __FILE__ ) ) ) . '/images/icon/pdf.png" border="0" alt="' . _MB_IMGLOSSARY_PDFTERM . '" />
@@ -79,21 +78,21 @@ function b_entries_random_show() {
 		// Functional links
 		if ( icms::$user ) {
 			if ( icms::$user -> isAdmin() ) {
-				$adminlinks = '<a href="' . ICMS_URL . '/modules/' . basename( dirname( dirname( __FILE__ ) ) ) . '/admin/index.php"><img src="' . ICMS_URL . '/modules/' . basename( dirname( dirname( __FILE__ ) ) ) . '/images/icon/computer.png" border="0" alt="' . _MB_IMGLOSSARY_ADMININDEX . '" /></a>';
-				$adminlinks .= '<a href="' . ICMS_URL . '/modules/' . basename( dirname( dirname( __FILE__ ) ) ) . '/admin/entry.php?op=mod&entryID=' . $entryID . '"><img src="' . ICMS_URL . '/modules/' . basename( dirname( dirname( __FILE__ ) ) ) . '/images/icon/edit.png" border="0" alt="' . _MB_IMGLOSSARY_EDITTERM . '" /></a>';
+				$adminlinks = '<a href="' . ICMS_URL . '/modules/' . basename( dirname( dirname( __FILE__ ) ) ) . '/admin/index.php"><img src="' . ICMS_URL . '/modules/' . basename( dirname( dirname( __FILE__ ) ) ) . '/images/icon/computer.png" border="0" alt="' . _MB_IMGLOSSARY_ADMININDEX . '" /></a>&nbsp;';
+				$adminlinks .= '<a href="' . ICMS_URL . '/modules/' . basename( dirname( dirname( __FILE__ ) ) ) . '/admin/entry.php?op=mod&entryID=' . $entryID . '"><img src="' . ICMS_URL . '/modules/' . basename( dirname( dirname( __FILE__ ) ) ) . '/images/icon/edit.png" border="0" alt="' . _MB_IMGLOSSARY_EDITTERM . '" /></a>&nbsp;';
 				$adminlinks .= '<a href="' . ICMS_URL . '/modules/' . basename( dirname( dirname( __FILE__ ) ) ) . '/admin/entry.php?op=del&entryID=' . $entryID . '"><img src="' . ICMS_URL . '/modules/' . basename( dirname( dirname( __FILE__ ) ) ) . '/images/icon/delete.png" border="0" alt="' . _MB_IMGLOSSARY_DELTERM . '" /></a>';
 			}
 		}
-		
+
 		$block['dirname'] = basename( dirname( dirname( __FILE__ ) ) );
 		if ( $imgConfig['multicats'] == 1 ) {
 			$block['category'] =  $category;
 			$block['term'] = '<h4 class="term">' . $term . '</h4>';
-			$block['definition'] =	'<div style="padding: 4px;">' . $definition . '</div>';
+			$block['definition'] = '<div style="padding: 4px;">' . $definition . '</div>';
 			$block['seemore'] = '<div style="font-size: x-small; padding: 4px;">
 									<a href="' . ICMS_URL . '/modules/' . basename( dirname( dirname( __FILE__ ) ) ) . '/index.php">' . _MB_IMGLOSSARY_SEEMORE . '</a>
 								 </div>';
-			$block['icons'] = '<div style="padding: 4px;">' . $adminlinks . $userlinks . '</div>';			
+			$block['icons'] = '<div style="padding: 4px;">' . $adminlinks . $userlinks . '</div>';
 		} else {
 			$block['term'] = '<h4 class="term">' . $term . '</h4>';
 			$block['definition'] = '<div style="padding: 4px;">' . $definition . '</div>';
