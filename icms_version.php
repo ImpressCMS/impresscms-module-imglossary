6c99cccd680fcd949098691c8ed3503d26b46b46
Ronald <pietjebell31@hotmail.com>
2012-02-19 11:58:30 +0000

    Setting status to RC-2 and set release date
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imglossary/trunk/modules/imglossary@23613 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- icms_version.php
+++ icms_version.php
@@ -34,9 +34,9 @@ if ( file_exists( ICMS_ROOT_PATH . '/modules/' . basename( dirname( __FILE__ ) )
 $modversion = array(
 	'name'				=> _MI_IMGLOSSARY_MD_NAME,
 	'version'			=> 1.03,
-	'status'			=> 'Trunk',
-	'status_version'	=> 'Trunk',
-	'date'				=> 'xx xxxxxx 2012',
+	'status'			=> 'RC-2',
+	'status_version'	=> 'RC-2',
+	'date'				=> '19 February 2012',
 
 	'description'		=> _MI_IMGLOSSARY_MD_DESC,
 	'author'			=> 'McDonald',
